eddc35aed4a151078b2ba1d1a1faab64a0add308
Dustin Morrill <dmorrill10@gmail.com>
2016-05-16 14:24:40 -0600

    - Fix long line in uniform_random_agent.py - Remove my complicated dex rule based agent - Move uniform random agent test with dex state to an integration   test directory

--- lib/games_puzzles_algorithms/players/rule_based/uniform_random_agent.py
+++ lib/games_puzzles_algorithms/players/rule_based/uniform_random_agent.py
@@ -1,4 +1,5 @@
-from games_puzzles_algorithms.choose import choose_legal_action_uniformly_randomly
+from games_puzzles_algorithms.choose \
+    import choose_legal_action_uniformly_randomly
 
 
 class UniformRandomAgent(object):
