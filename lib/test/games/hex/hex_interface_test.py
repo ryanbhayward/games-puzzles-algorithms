130c9266a5e0902127a2bf50a374d11fa3281cdc
Jesse Huard <jhuard@ualberta.ca>
2016-05-16 21:25:18 -0600

    Add test for hex coordinate system.

--- lib/test/games/hex/hex_interface_test.py
+++ lib/test/games/hex/hex_interface_test.py
@@ -28,6 +28,22 @@ def test_hex_move_made_twice():
     assert 'stone already on cell' in str(excinfo.value)
 
 
+def test_hex_play_move():
+    patient = GameState.root(6, 5)
+    patient.play(patient.board.cell_index(1, 2))
+    assert str(patient) == (
+'''
+  A  B  C  D  E
+1  .  .  .  .  .  O
+ 2  .  .  O  .  .  O
+  3  .  .  .  .  .  O
+   4  .  .  .  .  .  O
+    5  .  .  .  .  .  O
+     6  .  .  .  .  .  O
+         @  @  @  @  @'''
+    )
+
+
 def test_hex_undo():
     patient = GameState.root(5)
     patient.play(patient.board.cell_index(0, 0))
