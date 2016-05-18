3c04aecd84aa73a67a588de6cbe478d3de0822bf
Isabel McCarten <Isabel McCarten>
2016-05-18 16:47:55 -0600

    Change main to take puzzle and solver as arguments.

--- lib/games_puzzles_algorithms/ui/main.py
+++ lib/games_puzzles_algorithms/ui/main.py
@@ -1,22 +1,27 @@
 import argparse
 from interface import Interface
-
+from games_puzzles_algorithms.puzzles.sliding_tile_puzzle import SlidingTilePuzzle
+from games_puzzles_algorithms.search.a_star import AStar
+import sys
 
 def main():
-    """
-    Main function to get and respond to user input.
-    """
-    parser = argparse.ArgumentParser(
-        description='Run the CMPUT 396 puzzles Currently sliding_tile and maze are defined.',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-    parser.add_argument('--puzzle', type=str, default='maze', help='puzzle to run')
-    parser.add_argument('--search', type=str, default='A*', help='search algorithm to use')
-    parser.add_argument('--verbose', action='store_true', help='enable verbose mode to be true')
-
-    args = parser.parse_args()
-    interface = Interface(args.puzzle, args.search, args.verbose)
-    interface.prompt = ">"
+    """Main function to get and respond to user input."""
+    puzzle = 'sliding_tile'
+    solver = 'A*'
+    if len(sys.argv) > 1:
+	if sys.argv[1] in Interface.PUZZLES.keys():
+	    puzzle = sys.argv[1]
+	else:
+	    print('Error: invalid puzzle name')
+	    return
+    if len(sys.argv) > 2:
+	if sys.argv[2] in Interface.SOLVERS.keys():
+	    solver = sys.argv[2]
+	else:
+	    print('Error: invalid solver name')
+	    return
+    interface = Interface(puzzle, solver)
+    interface.prompt = '\n'
     interface.cmdloop()
 
 
