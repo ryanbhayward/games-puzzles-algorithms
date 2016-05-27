21da46413034a395d6fa972975f4fa7607022199
Jesse Huard <jhuard@ualberta.ca>
2016-05-26 20:06:44 -0600

    Add function to generate a hexagon centered on a point.

--- web/hex/static/js/main.js
+++ web/hex/static/js/main.js
@@ -8,6 +8,24 @@ var frame_height = 600;
 var game = new Phaser.Game(frame_width, frame_height, Phaser.Auto, "body",
                            {preload: preload, create: create,});
 
+var size = 50;
+
+// Generate a hexagon centered at the given center point.
+function hexagon(center) {
+    var point = center.clone();
+    var corners = [];
+
+    point.add(0, size);
+    corners.push(point.clone());
+
+    for (var i = 0; i < 5; ++i) {
+        point.rotate(center.x, center.y, 60, true);
+        corners.push(point.clone());
+    }
+
+    return new Phaser.Polygon(corners);
+}
+
 function preload() {
     // Set the game to scale automatically.
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
