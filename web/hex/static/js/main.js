053032ea97cbeda50e3c204f70f0bc1b1d485105
Jesse Huard <jhuard@ualberta.ca>
2016-05-26 20:01:13 -0600

    Add basic Phaser game.

---
+++ web/hex/static/js/main.js
@@ -0,0 +1,16 @@
+var frame_width = 1000;
+var frame_height = 600;
+
+var game = new Phaser.Game(frame_width, frame_height, Phaser.Auto, "body",
+                           {preload: preload, create: create,});
+
+function preload() {
+    // Set the game to scale automatically.
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+}
+
+function create() {
+    game.stage.backgroundColor = 0xffffff;
+}
