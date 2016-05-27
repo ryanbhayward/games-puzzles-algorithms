8e7d2122d7c928720fd787274e8c5ddbbe3318b9
Jesse Huard <jhuard@ualberta.ca>
2016-05-26 19:35:35 -0600

    Add default board state to application.

--- web/hex/application.py
+++ web/hex/application.py
@@ -1,7 +1,11 @@
 from flask import Flask, g, jsonify, render_template, request
 
+import games_puzzles_algorithms.games.hex.game_state as game
 
 app = Flask(__name__)
+column_dimension = 12
+row_dimension = 12
+state = game.GameState.root(row_dimension, column_dimension)
 
 
 @app.route('/')
