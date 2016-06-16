befce493a435b389c4e9b91418231422fe53071c
Dustin Morrill <dmorrill10@gmail.com>
2016-06-16 07:41:27 -0600

    Add GTP module that allows executable to run, but it's mostly copied and pasted from an old project and actions can't be played

--- lib/bin/games_puzzles_algorithms_gtp.py
+++ lib/bin/games_puzzles_algorithms_gtp.py
@@ -2,12 +2,12 @@
 
 import random
 import optparse
-from games_puzzles_algorithms.htp import HtpInterface
-from games_puzzles_algorithms.agent.rule_based_agent import CombinedPlayerRuleBasedAgent
+from games_puzzles_algorithms.games.hex.htp import HtpInterface
+from games_puzzles_algorithms.players.rule_based.uniform_random_agent import UniformRandomAgent
 
 
 def agent(r, **kwargs):
-    return CombinedPlayerRuleBasedAgent(lambda: r.uniform(0, 1), **kwargs)
+    return UniformRandomAgent(lambda: r.uniform(0, 1), **kwargs)
 
 
 if __name__ == '__main__':
@@ -24,26 +24,10 @@ if __name__ == '__main__':
         dest="script",
         help="A script of commands to play out."
     )
-    parser.add_option(
-        "-u",
-        "--uct",
-        dest="only_search",
-        action="store_true",
-        default=False,
-        help="Use a RAVE enabled UCT agent. Defaults to False."
-    )
-    parser.add_option(
-        "-c",
-        "--only-corners",
-        dest="only_corners",
-        action="store_true",
-        default=True,
-        help="Play only in the obtuse corners on the first move instead of anywhere on the main diagonal. Defaults to True."
-    )
     (options, args) = parser.parse_args()
     r = random.Random(options.random_seed)
     try:
-        cli = HtpInterface(agent(r, only_corners=options.only_corners, only_search=options.only_search))
+        cli = HtpInterface(agent(r))
         if options.script is not None:
             with open(options.script) as f:
                 for command in f:
