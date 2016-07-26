524df73407390197b436d1a8082afc3afa89cde8
Jesse Huard <jhuard@ualberta.ca>
2016-07-26 00:43:20 -0600

    Add basic tournament between GTP players.

---
+++ tournament/play_tournament.py
@@ -0,0 +1,68 @@
+import argparse
+import logging
+import sys
+
+from player import Player
+from tournament import Tournament
+
+
+def tournament_args():
+    parser = argparse.ArgumentParser('Play a tournament between two GTP '
+                                     'players.')
+    parser.add_argument('players', nargs=2,
+                        help='Invocation for each GTP player.')
+    parser.add_argument('-n', '--num-games', type=int, default=1,
+                        help='Number of games in tournament.')
+    parser.add_argument('-s', '--size', type=int, default=3,
+                        help='Size of game instance.')
+    parser.add_argument('-t', '--time-limit', type=int, default=1,
+                        help='Time limit for each move, in seconds.')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Use verbose output for stdout.')
+    parser.add_argument('-l', '--log-file', help='File to store logs.')
+    return parser.parse_args()
+
+
+def tournament_logger(verbose=False, log_file=None):
+    """Creates and configures a logger for use with the tournament."""
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(message)s\n')
+
+    if log_file is not None:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(formatter)
+
+    if verbose:
+        stdout_handler.setLevel(logging.DEBUG)
+    else:
+        stdout_handler.setLevel(logging.INFO)
+
+    logger.addHandler(stdout_handler)
+
+    return logger
+
+
+def main():
+    args = tournament_args()
+
+    logger = tournament_logger(verbose=args.verbose, log_file=args.log_file)
+
+    players = [Player(invocation) for invocation in args.players]
+
+    tournament = Tournament(players, args.num_games, args.size,
+                            args.time_limit, logger)
+
+    tournament.play_tournament()
+
+    for p in players:
+        p.exit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tournament/tournament.py
@@ -0,0 +1,125 @@
+from collections import defaultdict
+
+
+class TournamentResult(object):
+    """
+    TournamentResult holds the result of a tournament. This is defined as a
+    count of how many times each player has won, the count of how many draws
+    have occurred, and the sequence of wins as they occurred.
+    """
+    def __init__(self):
+        self._sequence = []
+        self._results = defaultdict(int)
+        self._draws = 0
+
+    def increment_win_count(self, player, side):
+        # Check for a draw.
+        if side == '0':
+            self._sequence.append(('Draw', '0'))
+            self._draws += 1
+        else:
+            self._sequence.append((player, side))
+            self._results[player] += 1
+
+    def __str__(self):
+        outcomes = []
+
+        for (player, games) in self._results.items():
+            outcomes.append('{} won {} games.'.format(player, games))
+
+        if self._draws > 0:
+            outcomes.append('{} draws occurred.'.format(self._draws))
+
+        return '\n'.join(outcomes)
+
+
+class Tournament(object):
+    """
+    Tournament is used to run a tournament between two GTP players. The
+    tournament will handle configuring the players as necessary, and will then
+    play a series of rounds between the players. The first player to play
+    alternates between players each round. A round consists of both players
+    making moves until they agree that the game is over. We make the assumption
+    that players will agree the game is over after a winning move has been made.
+    """
+    def __init__(self, players, num_games, game_size, time_limit, logger):
+        self._logger = logger
+        self._results = TournamentResult()
+        self._round = 1
+        self._players = players
+        self._games = num_games
+        self._size = game_size
+        self._time_limit = time_limit
+        self._configure_players()
+
+    def _configure_players(self):
+        """Send configuration to each player"""
+        for player in self._players:
+            player.configure(size=self._size, time_limit=self._time_limit)
+
+    def _round_finished(self):
+        """Ask if players agree the game is finished."""
+        return all(p.game_finished() for p in self._players)
+
+    def _notify_players(self, move, skip_index):
+        """
+        Notify all other players of the given move, skipping the player at
+        index.
+        """
+        for (i, player) in enumerate(self._players):
+            if i == skip_index:
+                continue
+            player.play(move)
+
+    def _next_player(self, player_index):
+        """Return the next to play after the player at the given index."""
+        num_players = len(self._players)
+        return (player_index + 1) % num_players
+
+    def _play_round(self, first_to_play):
+        """Play a single round of a tournament."""
+        for player in self._players:
+            player.clear()
+
+        player_index = first_to_play
+
+        while not self._round_finished():
+            player = self._players[player_index]
+            move = player.play()
+
+            self._logger.debug('{} plays {}\n{}'.format(player, move,
+                                                        player.board()))
+
+            self._notify_players(move, player_index)
+            player_index = self._next_player(player_index)
+
+        (winner, _) = player.winner()
+
+        # We currently make the assumption that wins are detected immediately
+        # by both players, and thus the last player to act is the winner if a
+        # draw has not occurred.
+        self._results.increment_win_count(str(player), winner)
+        self._logger.debug('{} wins round {} as {}'.format(player, self._round,
+                                                           winner))
+
+        self._round += 1
+
+    def play_tournament(self):
+        """Play a full tournament between the players."""
+        self._logger.info(self)
+        self._round = 1
+
+        first_to_play = 0
+        for _ in range(self._games):
+            self._play_round(first_to_play)
+            first_to_play = self._next_player(first_to_play)
+
+        self._logger.info(self._results)
+
+    def __str__(self):
+        players = ' vs. '.join(str(p) for p in self._players)
+        size = 'Size: {}'.format(self._size)
+        time = 'Time limit: {}s'.format(self._time_limit)
+        games = 'Games: {}'.format(self._games)
+
+        return '\n'.join((players, size, time, games))
