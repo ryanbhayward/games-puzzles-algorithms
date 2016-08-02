994bcdb2377fe171dd3483e310a0dfc39d7847cf
Jesse Huard <jhuard@ualberta.ca>
2016-08-01 19:15:47 -0600

    Add MIT license to top-level setup.py.

--- lib/setup.py
+++ lib/setup.py
@@ -4,6 +4,7 @@ import warnings
 setup(
     name='games_puzzles_algorithms',
     version='0.0.1',
+    license='MIT',
     packages=find_packages(),
     scripts=['bin/gpa-games-cli',
              #'bin/gpa-puzzles-cli'
@@ -13,5 +14,8 @@ setup(
         'setuptools == 20.2.2'
     ],
     tests_require=['pytest'],
-    setup_requires=['pytest-runner']
+    setup_requires=['pytest-runner'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+    ],
 )
