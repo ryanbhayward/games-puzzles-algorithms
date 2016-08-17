51ca7609bacf899c9c8996c979c16653573fe5f6
Alexander Wong <admin@alexander-wong.com>
2016-08-16 19:34:23 -0600

    Added tournament compatible dumb_michi

---
+++ michi/dumb_michi.py
@@ -0,0 +1,501 @@
+#!/usr/bin/env pypy
+# -*- coding: utf-8 -*-
+#
+# michi.py without pattern matching or Monte Carlo playout implementation.
+# Will work with the Go Text Protocol by choosing random valid moves.
+
+from __future__ import print_function
+from collections import namedtuple
+from itertools import count
+import random
+import re
+import sys
+
+
+# Given a board of size NxN (N=9, 19, ...), we represent the position
+# as an (N+1)*(N+2) string, with '.' (empty), 'X' (to-play player),
+# 'x' (other player), and whitespace (off-board border to make rules
+# implementation easier).  Coordinates are just indices in this string.
+# You can simply print(board) when debugging.
+N = 13
+W = N + 2
+empty = "\n".join([(N+1)*' '] + N*[' '+N*'.'] + [(N+2)*' '])
+colstr = 'ABCDEFGHJKLMNOPQRST'
+MAX_GAME_LEN = N * N * 3
+
+N_SIMS = 1400
+RAVE_EQUIV = 3500
+EXPAND_VISITS = 8
+PRIOR_EVEN = 10  # should be even number; 0.5 prior
+PRIOR_SELFATARI = 10  # negative prior
+PRIOR_CAPTURE_ONE = 15
+PRIOR_CAPTURE_MANY = 30
+PRIOR_PAT3 = 10
+PRIOR_LARGEPATTERN = 100  # most moves have relatively small probability
+PRIOR_CFG = [24, 22, 8]  # priors for moves in cfg dist. 1, 2, 3
+PRIOR_EMPTYAREA = 10
+REPORT_PERIOD = 200
+PROB_HEURISTIC = {'capture': 0.9, 'pat3': 0.95}  # probability of heuristic suggestions being taken in playout
+PROB_SSAREJECT = 0.9  # probability of rejecting suggested self-atari in playout
+PROB_RSAREJECT = 0.5  # probability of rejecting random self-atari in playout; this is lower than above to allow nakade
+RESIGN_THRES = 0.2
+FASTPLAY20_THRES = 0.8  # if at 20% playouts winrate is >this, stop reading
+FASTPLAY5_THRES = 0.95  # if at 5% playouts winrate is >this, stop reading
+
+pat3src = [  # 3x3 playout patterns; X,O are colors, x,o are their inverses
+       ["XOX",  # hane pattern - enclosing hane
+        "...",
+        "???"],
+       ["XO.",  # hane pattern - non-cutting hane
+        "...",
+        "?.?"],
+       ["XO?",  # hane pattern - magari
+        "X..",
+        "x.?"],
+       # ["XOO",  # hane pattern - thin hane
+       #  "...",
+       #  "?.?", "X",  - only for the X player
+       [".O.",  # generic pattern - katatsuke or diagonal attachment; similar to magari
+        "X..",
+        "..."],
+       ["XO?",  # cut1 pattern (kiri] - unprotected cut
+        "O.o",
+        "?o?"],
+       ["XO?",  # cut1 pattern (kiri] - peeped cut
+        "O.X",
+        "???"],
+       ["?X?",  # cut2 pattern (de]
+        "O.O",
+        "ooo"],
+       ["OX?",  # cut keima
+        "o.O",
+        "???"],
+       ["X.?",  # side pattern - chase
+        "O.?",
+        "   "],
+       ["OX?",  # side pattern - block side cut
+        "X.O",
+        "   "],
+       ["?X?",  # side pattern - block side connection
+        "x.O",
+        "   "],
+       ["?XO",  # side pattern - sagari
+        "x.x",
+        "   "],
+       ["?OX",  # side pattern - cut
+        "X.O",
+        "   "],
+       ]
+
+pat_gridcular_seq = [  # Sequence of coordinate offsets of progressively wider diameters in gridcular metric
+        [[0,0],
+         [0,1], [0,-1], [1,0], [-1,0],
+         [1,1], [-1,1], [1,-1], [-1,-1], ],  # d=1,2 is not considered separately
+        [[0,2], [0,-2], [2,0], [-2,0], ],
+        [[1,2], [-1,2], [1,-2], [-1,-2], [2,1], [-2,1], [2,-1], [-2,-1], ],
+        [[0,3], [0,-3], [2,2], [-2,2], [2,-2], [-2,-2], [3,0], [-3,0], ],
+        [[1,3], [-1,3], [1,-3], [-1,-3], [3,1], [-3,1], [3,-1], [-3,-1], ],
+        [[0,4], [0,-4], [2,3], [-2,3], [2,-3], [-2,-3], [3,2], [-3,2], [3,-2], [-3,-2], [4,0], [-4,0], ],
+        [[1,4], [-1,4], [1,-4], [-1,-4], [3,3], [-3,3], [3,-3], [-3,-3], [4,1], [-4,1], [4,-1], [-4,-1], ],
+        [[0,5], [0,-5], [2,4], [-2,4], [2,-4], [-2,-4], [4,2], [-4,2], [4,-2], [-4,-2], [5,0], [-5,0], ],
+        [[1,5], [-1,5], [1,-5], [-1,-5], [3,4], [-3,4], [3,-4], [-3,-4], [4,3], [-4,3], [4,-3], [-4,-3], [5,1], [-5,1], [5,-1], [-5,-1], ],
+        [[0,6], [0,-6], [2,5], [-2,5], [2,-5], [-2,-5], [4,4], [-4,4], [4,-4], [-4,-4], [5,2], [-5,2], [5,-2], [-5,-2], [6,0], [-6,0], ],
+        [[1,6], [-1,6], [1,-6], [-1,-6], [3,5], [-3,5], [3,-5], [-3,-5], [5,3], [-5,3], [5,-3], [-5,-3], [6,1], [-6,1], [6,-1], [-6,-1], ],
+        [[0,7], [0,-7], [2,6], [-2,6], [2,-6], [-2,-6], [4,5], [-4,5], [4,-5], [-4,-5], [5,4], [-5,4], [5,-4], [-5,-4], [6,2], [-6,2], [6,-2], [-6,-2], [7,0], [-7,0], ],
+    ]
+spat_patterndict_file = 'patterns.spat'
+large_patterns_file = 'patterns.prob'
+
+
+#######################
+# board string routines
+
+def neighbors(c):
+    """ generator of coordinates for all neighbors of c """
+    return [c-1, c+1, c-W, c+W]
+
+
+def diag_neighbors(c):
+    """ generator of coordinates for all diagonal neighbors of c """
+    return [c-W-1, c-W+1, c+W-1, c+W+1]
+
+
+def board_put(board, c, p):
+    return board[:c] + p + board[c+1:]
+
+
+def floodfill(board, c):
+    """ replace continuous-color area starting at c with special color # """
+    # This is called so much that a bytearray is worthwhile...
+    byteboard = bytearray(board)
+    p = byteboard[c]
+    byteboard[c] = ord('#')
+    fringe = [c]
+    while fringe:
+        c = fringe.pop()
+        for d in neighbors(c):
+            if byteboard[d] == p:
+                byteboard[d] = ord('#')
+                fringe.append(d)
+    return str(byteboard)
+
+
+# Regex that matches various kind of points adjecent to '#' (floodfilled) points
+contact_res = dict()
+for p in ['.', 'x', 'X']:
+    rp = '\\.' if p == '.' else p
+    contact_res_src = ['#' + rp,  # p at right
+                       rp + '#',  # p at left
+                       '#' + '.'*(W-1) + rp,  # p below
+                       rp + '.'*(W-1) + '#']  # p above
+    contact_res[p] = re.compile('|'.join(contact_res_src), flags=re.DOTALL)
+
+
+def contact(board, p):
+    """ test if point of color p is adjecent to color # anywhere
+    on the board; use in conjunction with floodfill for reachability """
+    m = contact_res[p].search(board)
+    if not m:
+        return None
+    return m.start() if m.group(0)[0] == p else m.end() - 1
+
+
+def is_eyeish(board, c):
+    """ test if c is inside a single-color diamond and return the diamond
+    color or None; this could be an eye, but also a false one """
+    eyecolor = None
+    for d in neighbors(c):
+        if board[d].isspace():
+            continue
+        if board[d] == '.':
+            return None
+        if eyecolor is None:
+            eyecolor = board[d]
+            othercolor = eyecolor.swapcase()
+        elif board[d] == othercolor:
+            return None
+    return eyecolor
+
+
+def is_eye(board, c):
+    """ test if c is an eye and return its color or None """
+    eyecolor = is_eyeish(board, c)
+    if eyecolor is None:
+        return None
+
+    # Eye-like shape, but it could be a falsified eye
+    falsecolor = eyecolor.swapcase()
+    false_count = 0
+    at_edge = False
+    for d in diag_neighbors(c):
+        if board[d].isspace():
+            at_edge = True
+        elif board[d] == falsecolor:
+            false_count += 1
+    if at_edge:
+        false_count += 1
+    if false_count >= 2:
+        return None
+
+    return eyecolor
+
+
+class Position(namedtuple('Position', 'board cap n ko last last2 komi')):
+    """ Implementation of simple Chinese Go rules;
+    n is how many moves were played so far """
+
+    def move(self, c):
+        """ play as player X at the given coord c, return the new position """
+
+        # Test for ko
+        if c == self.ko:
+            return None
+        # Are we trying to play in enemy's eye?
+        in_enemy_eye = is_eyeish(self.board, c) == 'x'
+
+        board = board_put(self.board, c, 'X')
+        # Test for captures, and track ko
+        capX = self.cap[0]
+        singlecaps = []
+        for d in neighbors(c):
+            if board[d] != 'x':
+                continue
+            # XXX: The following is an extremely naive and SLOW approach
+            # at things - to do it properly, we should maintain some per-group
+            # data structures tracking liberties.
+            fboard = floodfill(board, d)  # get a board with the adjecent group replaced by '#'
+            if contact(fboard, '.') is not None:
+                continue  # some liberties left
+            # no liberties left for this group, remove the stones!
+            capcount = fboard.count('#')
+            if capcount == 1:
+                singlecaps.append(d)
+            capX += capcount
+            board = fboard.replace('#', '.')  # capture the group
+        # Set ko
+        ko = singlecaps[0] if in_enemy_eye and len(singlecaps) == 1 else None
+        # Test for suicide
+        if contact(floodfill(board, c), '.') is None:
+            return None
+
+        # Update the position and return
+        return Position(board=board.swapcase(), cap=(self.cap[1], capX),
+                        n=self.n + 1, ko=ko, last=c, last2=self.last, komi=self.komi)
+
+    def pass_move(self):
+        """ pass - i.e. return simply a flipped position """
+        return Position(board=self.board.swapcase(), cap=(self.cap[1], self.cap[0]),
+                        n=self.n + 1, ko=None, last=None, last2=self.last, komi=self.komi)
+
+    def moves(self, i0):
+        """ Generate a list of moves (includes false positives - suicide moves;
+        does not include true-eye-filling moves), starting from a given board
+        index (that can be used for randomization) """
+        i = i0-1
+        passes = 0
+        while True:
+            i = self.board.find('.', i+1)
+            if passes > 0 and (i == -1 or i >= i0):
+                break  # we have looked through the whole board
+            elif i == -1:
+                i = 0
+                passes += 1
+                continue  # go back and start from the beginning
+            # Test for to-play player's one-point eye
+            if is_eye(self.board, i) == 'X':
+                continue
+            yield i
+
+    def last_moves_neighbors(self):
+        """ generate a randomly shuffled list of points including and
+        surrounding the last two moves (but with the last move having
+        priority) """
+        clist = []
+        for c in self.last, self.last2:
+            if c is None:  continue
+            dlist = [c] + list(neighbors(c) + diag_neighbors(c))
+            random.shuffle(dlist)
+            clist += [d for d in dlist if d not in clist]
+        return clist
+
+    def score(self, owner_map=None):
+        """ compute score for to-play player; this assumes a final position
+        with all dead stones captured; if owner_map is passed, it is assumed
+        to be an array of statistics with average owner at the end of the game
+        (+1 black, -1 white) """
+        board = self.board
+        i = 0
+        while True:
+            i = self.board.find('.', i+1)
+            if i == -1:
+                break
+            fboard = floodfill(board, i)
+            # fboard is board with some continuous area of empty space replaced by #
+            touches_X = contact(fboard, 'X') is not None
+            touches_x = contact(fboard, 'x') is not None
+            if touches_X and not touches_x:
+                board = fboard.replace('#', 'X')
+            elif touches_x and not touches_X:
+                board = fboard.replace('#', 'x')
+            else:
+                board = fboard.replace('#', ':')  # seki, rare
+            # now that area is replaced either by X, x or :
+        komi = self.komi if self.n % 2 == 1 else -self.komi
+        if owner_map is not None:
+            for c in range(W*W):
+                n = 1 if board[c] == 'X' else -1 if board[c] == 'x' else 0
+                owner_map[c] += n * (1 if self.n % 2 == 0 else -1)
+        return board.count('X') - board.count('x') + komi
+
+
+def empty_position():
+    """ Return an initial board position """
+    return Position(board=empty, cap=(0, 0), n=0, ko=None, last=None, last2=None, komi=7.5)
+
+
+###################
+# user interface(s)
+
+# utility routines
+
+def print_pos(pos, f=sys.stderr, owner_map=None):
+    """ print visualization of the given board position, optionally also
+    including an owner map statistic (probability of that area of board
+    eventually becoming black/white) """
+    if pos.n % 2 == 0:  # to-play is black
+        board = pos.board.replace('x', 'O')
+        Xcap, Ocap = pos.cap
+    else:  # to-play is white
+        board = pos.board.replace('X', 'O').replace('x', 'X')
+        Ocap, Xcap = pos.cap
+    print('Move: %-3d   Black: %d caps   White: %d caps  Komi: %.1f' % (pos.n, Xcap, Ocap, pos.komi), file=f)
+    pretty_board = ' '.join(board.rstrip()) + ' '
+    if pos.last is not None:
+        pretty_board = pretty_board[:pos.last*2-1] + '(' + board[pos.last] + ')' + pretty_board[pos.last*2+2:]
+    rowcounter = count()
+    pretty_board = [' %-02d%s' % (N-i, row[2:]) for row, i in zip(pretty_board.split("\n")[1:], rowcounter)]
+    if owner_map is not None:
+        pretty_ownermap = ''
+        for c in range(W*W):
+            if board[c].isspace():
+                pretty_ownermap += board[c]
+            elif owner_map[c] > 0.6:
+                pretty_ownermap += 'X'
+            elif owner_map[c] > 0.3:
+                pretty_ownermap += 'x'
+            elif owner_map[c] < -0.6:
+                pretty_ownermap += 'O'
+            elif owner_map[c] < -0.3:
+                pretty_ownermap += 'o'
+            else:
+                pretty_ownermap += '.'
+        pretty_ownermap = ' '.join(pretty_ownermap.rstrip())
+        pretty_board = ['%s   %s' % (brow, orow[2:]) for brow, orow in zip(pretty_board, pretty_ownermap.split("\n")[1:])]
+    print("\n".join(pretty_board), file=f)
+    print('    ' + ' '.join(colstr[:N]), file=f)
+    print('', file=f)
+
+
+def dump_subtree(node, thres=N_SIMS/50, indent=0, f=sys.stderr, recurse=True):
+    """ print this node and all its children with v >= thres. """
+    print("%s+- %s %.3f (%d/%d, prior %d/%d, rave %d/%d=%.3f, urgency %.3f)" %
+          (indent*' ', str_coord(node.pos.last), node.winrate(),
+           node.w, node.v, node.pw, node.pv, node.aw, node.av,
+           float(node.aw)/node.av if node.av > 0 else float('nan'),
+           node.rave_urgency()), file=f)
+    if not recurse:
+        return
+    for child in sorted(node.children, key=lambda n: n.v, reverse=True):
+        if child.v >= thres:
+            dump_subtree(child, thres=thres, indent=indent+3, f=f)
+
+
+def print_tree_summary(tree, sims, f=sys.stderr):
+    best_nodes = sorted(tree.children, key=lambda n: n.v, reverse=True)[:5]
+    best_seq = []
+    node = tree
+    while node is not None:
+        best_seq.append(node.pos.last)
+        node = node.best_move()
+    print('[%4d] winrate %.3f | seq %s | can %s' %
+          (sims, best_nodes[0].winrate(), ' '.join([str_coord(c) for c in best_seq[1:6]]),
+           ' '.join(['%s(%.3f)' % (str_coord(n.pos.last), n.winrate()) for n in best_nodes])), file=f)
+
+
+def parse_coord(s):
+    if s == 'pass':
+        return None
+    return W+1 + (N - int(s[1:])) * W + colstr.index(s[0].upper())
+
+
+def str_coord(c):
+    if c is None:
+        return 'pass'
+    row, col = divmod(c - (W+1), W)
+    return '%c%d' % (colstr[col], N - row)
+
+
+# various main programs
+
+def tournament_io():
+    """ Tournament interface for our program.  We can play only on the board size
+    which is configured (N), and we ignore color information and assume
+    alternating play! """
+    # instead of a tree, represent the game as an array of Position
+    state = [empty_position()]
+
+    while True:
+        try:
+            line = raw_input().strip()
+        except EOFError:
+            break
+        if line == '':
+            continue
+        command = [s.lower() for s in line.split()]
+        if re.match('\d+', command[0]):
+            cmdid = command[0]
+            command = command[1:]
+        else:
+            cmdid = ''
+        owner_map = W*W*[0]
+        ret = ''
+
+        if command[0] == "name":
+            ret = "michi"
+        elif command[0] == "size":
+            if int(command[1]) != N:
+                print("Warning: Trying to set incompatible boardsize %s (!= %d)" % (command[1], N), file=sys.stderr)
+                ret = None
+        elif command[0] == "set_time":
+            pass
+        elif command[0] == 'clear_board':
+            state = list()
+            state.append(empty_position())
+        elif command[0] == "genmove":
+            if state[-1] is None:
+                ret = 'resign'
+            else:
+                x, y = random.randint(1, N), random.randint(1, N)
+                possible_moves = list(state[-1].moves(y * W + x))
+                if len(possible_moves) > 0:
+                    ret = str_coord(possible_moves[0])
+                    state.append(state[-1].move(possible_moves[0]))
+                else:
+                    ret = 'pass'
+        elif command[0] == "play":
+            if command[1] == "resign":
+                state.append(state[-1].pass_move())
+            elif state[-1] is not None:
+                c = parse_coord(command[1])
+                if c is not None:
+                    state.append(state[-1].move(c))
+                else:
+                    # pass the turn
+                    state.append(state[-1].pass_move())
+        elif command[0] == "winner":
+            # dumb way of calculating 'winners', if there are still valid moves continue playing
+            if state[-1] is None:
+                score = state[-2].score()
+                if state[-2].n % 2:
+                    score = -score
+                if score == 0:
+                    ret = '0'
+                elif score > 0:
+                    ret = 'B+%.1f' % (score,)
+                elif score < 0:
+                    ret = 'W+%.1f' % (score,)
+            else:
+                x, y = random.randint(1, N), random.randint(1, N)
+                possible_moves = list(state[-1].moves(y * W + x))
+                if len(possible_moves) > 0:
+                    ret = ''
+                else:
+                    score = state[-1].score()
+                    if state[-1].n % 2:
+                        score = -score
+                    if score == 0:
+                        ret = ''
+                    elif score > 0:
+                        ret = 'B+%.1f' % (score,)
+                    elif score < 0:
+                        ret = 'W+%.1f' % (score,)
+        elif command[0] == "showboard":
+            if state[-1] is None:
+                print_pos(state[-2], sys.stderr, owner_map)
+            else:
+                print_pos(state[-1], sys.stderr, owner_map)
+        elif command[0] == "q":
+            return
+        else:
+            print("Warning: Ignoring unknown command - %s" % (line,), file=sys.stderr)
+            ret = None
+
+        if ret is not None:
+            print('=%s %s\n\n' % (cmdid, ret,), end='')
+        else:
+            print('?%s ???\n\n' % (cmdid,), end='')
+        sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    tournament_io()
