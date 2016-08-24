66318f128af2f551be33036dc500cdce8dc7a336
Jesse Huard <jhuard@ualberta.ca>
2016-08-23 23:25:01 -0600

    TTT: Modify win detection to handle k-in-a-row.

--- lib/games_puzzles_algorithms/games/ttt/game_state.py
+++ lib/games_puzzles_algorithms/games/ttt/game_state.py
@@ -72,6 +72,137 @@ class TwoDimensionalTable(object):
         return (i for i in self._data)
 
 
+class WinDetector(object):
+    """
+    The WinDetector is used to track the win state of a game of tic-tac-toe.
+    This is done by tracking run-lengths on the board for each direction. We
+    perform this count from left to right and top to bottom along the board.
+    The value in any given cell if the length of the run ending in that cell.
+    """
+    def __init__(self, rows, columns, spaces_to_win):
+        self.rows = rows
+        self.columns = columns
+        self.spaces_to_win = spaces_to_win
+        self._rows = TwoDimensionalTable(rows, columns)
+        self._columns = TwoDimensionalTable(rows, columns)
+        self._diagonals = TwoDimensionalTable(rows, columns)
+        self._anti_diagonals = TwoDimensionalTable(rows, columns)
+
+    def _iter_row(self, row, column):
+        """Iterate along a row starting at the given coordinate."""
+        for c in range(column, self.columns):
+            yield (row, c)
+
+    def _iter_column(self, row, column):
+        """Iterate along a column starting at the given coordinate."""
+        for r in range(row, self.rows):
+            yield (r, column)
+
+    def _iter_diagonal(self, row, column):
+        """Iterate along a diagonal starting at the given coordinate."""
+        row_difference = self.rows - row
+        column_difference = self.columns - column
+
+        for i in range(min(row_difference, column_difference)):
+            yield(row + i, column + i)
+
+    def _iter_anti_diagonal(self, row, column):
+        """Iterate along an anti-diagonal starting at the given coordinate."""
+        row_difference = self.rows - row
+        for i in range(min(row_difference, column)):
+            yield(row - i, column - i)
+
+    def _cascade_state_update(self, state, row, column, iterator):
+        """
+        Starting from the given coordinate, increment the run-length along the
+        direction defined by the iterator until we hit a blank spot on the
+        board.
+        """
+        run_length = state[row, column]
+
+        for r, c in iterator(row, column):
+            # A zero run-length indicates no piece in this cell.
+            if state[r, c] == 0:
+                break
+
+            state[r, c] = run_length
+            run_length += 1
+
+    def _row_length(self, row, column):
+        """
+        Calculate the existing run-length in this row ending at the given
+        coordinate.
+        """
+        return 0 if column == 0 else self._rows[row, column - 1]
+
+    def _column_length(self, row, column):
+        """
+        Calculate the existing run-length in this column ending at the given
+        coordinate.
+        """
+        return 0 if row == 0 else self._columns[row - 1, column]
+
+    def _diagonal_length(self, row, column):
+        """
+        Calculate the existing run-length in this diagonal ending at the given
+        coordinate.
+        """
+        if row == 0 or column == 0:
+            return 0
+        else:
+            return self._diagonals[row - 1, column - 1]
+
+    def _anti_diagonal_length(self, row, column):
+        """
+        Calculate the existing run-length in this anti-diagonal ending at the
+        given coordinate.
+        """
+        if row == 0 or column == self.columns - 1:
+            return 0
+        else:
+            return self._anti_diagonals[row - 1, column + 1]
+
+    def _update_triples(self):
+        """
+        A generator over the triples needed to update the WinDetector's
+        states.
+        """
+        yield (self._rows, self._row_length, self._iter_row)
+        yield (self._columns, self._column_length, self._iter_column)
+        yield (self._diagonals, self._diagonal_length, self._iter_diagonal)
+        yield (self._anti_diagonals, self._anti_diagonal_length,
+               self._iter_anti_diagonal)
+
+    def update(self, move, is_undo=False):
+        """
+        Update the WinDetector states to account for the given move. If the move
+        was an undo, we set the run-length of the given cell to zero. Otherwise,
+        we extend the existing run ending at this cell. Afterwards, we propagate
+        the updated run to the cells below.
+        """
+        # All states are equally valid to query for the move coordinates.
+        row = self._rows.row(move)
+        column = self._rows.column(move)
+
+        for (state, run_length, iterator) in self._update_triples():
+            state[row, column] = 0 if is_undo else run_length(row, column) + 1
+            self._cascade_state_update(state, row, column, iterator)
+
+    def _states(self):
+        """A generator of the WinDetector states."""
+        yield self._rows
+        yield self._columns
+        yield self._diagonals
+        yield self._anti_diagonals
+
+    def win_detected(self):
+        """Return whether any runs exist that meet the winning length."""
+        for state in self._states():
+            if any(run_length >= self.spaces_to_win for run_length in state):
+                return True
+        return False
+
+
 class Board(object):
 
     def __init__(self, num_rows=3, num_columns=None, num_spaces_to_win=None):
@@ -85,9 +216,13 @@ class Board(object):
             num_columns,
             initial_elem=BoardValues.Empty,
             elem_type='b')
-        
+
         self._actions = []
-        self.initialize_win_status()
+        self._win_detectors = {
+            BoardValues.X: WinDetector(num_rows, num_columns,
+                                       num_spaces_to_win),
+            BoardValues.O: WinDetector(num_rows, num_columns, num_spaces_to_win)
+        }
 
     def __str__(self):
         rows = self._spaces.num_rows()
@@ -139,7 +274,7 @@ class Board(object):
                              "player!")
         self._spaces.set_index(action, player)
         self._actions.append({'player': player, 'action': action})
-        self._update_win_status(action, player, increment=True)
+        self._win_detectors[player].update(action)
 
     def undo(self):
         if len(self._actions) == 0:
@@ -147,47 +282,30 @@ class Board(object):
 
         last_action = self._actions.pop()
         self._spaces.set_index(last_action['action'], BoardValues.Empty)
-        self._update_win_status(last_action['action'],
-                                last_action['player'], increment=False)
+        self._win_detectors[last_action['player']].update(last_action['action'],
+                                                          is_undo=True)
         return last_action['player']
 
-    def space_is_on_positive_diagonal(self, row, column):
-        return row == column
-
-    def space_is_on_negative_diagonal(self, row, column):
-        return row == (self._spaces.num_columns() - (column + 1))
-
-    def initialize_win_status(self):
-        self._status = {}
-        self._status[BoardValues.X] = {'positive_diagonal': 0,
-                                       'negative_diagonal': 0}
-
-        for row in range(self._spaces.num_rows()):
-            self._status[BoardValues.X]['row' + str(row)] = 0
-
-        for column in range(self._spaces.num_columns()):
-            self._status[BoardValues.X]['column' + str(column)] = 0
-
-        self._status[BoardValues.O] = self._status[BoardValues.X].copy()
-
-    def _update_win_status(self, action, player, increment=True):
-        row = self._spaces.row(action)
-        column = self._spaces.column(action)
-
-        modifier = 1 if increment else -1
-
-        self._status[player]['row' + str(row)] += modifier
-        self._status[player]['column' + str(column)] += modifier
-
-        if self.space_is_on_positive_diagonal(row, column):
-            self._status[player]['positive_diagonal'] += modifier
-
-        if self.space_is_on_negative_diagonal(row, column):
-            self._status[player]['negative_diagonal'] += modifier
-
     def _has_win(self, player):
-        player_status = self._status[player]
-        return any(n == self._num_spaces_to_win for n in player_status.values())
+        return self._win_detectors[player].win_detected()
+
+    def num_spaces_to_win(self): return self._num_spaces_to_win
+
+    def winner(self):
+        '''
+        Returns: None if the game is unfinished
+                 BoardValues.X if the x player has won
+                 BoardValues.O if the o player has won
+                 BoardValues.Empty if the game is a draw
+        '''
+        if self._has_win(BoardValues.X):
+            return BoardValues.X
+        elif self._has_win(BoardValues.O):
+            return BoardValues.O
+        elif self.num_empty_spaces() == 0:
+            return BoardValues.Empty
+        else:
+            return None
 
     def heuristic(self, player):
         """
@@ -223,25 +341,6 @@ class Board(object):
         rows = self._spaces.num_rows()
         scale = cols * rows * 2 + max(cols, rows) * 2
         return value / scale
-
-
-    def num_spaces_to_win(self): return self._num_spaces_to_win
-
-    def winner(self):
-        '''
-        Returns: None if the game is unfinished
-                 BoardValues.X if the x player has won
-                 BoardValues.O if the o player has won
-                 BoardValues.Empty if the game is a draw
-        '''
-        if self._has_win(BoardValues.X):
-            return BoardValues.X
-        elif self._has_win(BoardValues.O):
-            return BoardValues.O
-        elif self.num_empty_spaces() == 0:
-            return BoardValues.Empty
-        else:
-            return None
 
 
 class GameState(Board):
