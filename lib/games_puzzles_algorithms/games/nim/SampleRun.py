7c9053c308d28a7095a63db92b5f617e0094367d
Jesse Huard <jhuard@ualberta.ca>
2016-09-04 07:52:59 -0600

    NIM: Fix sample issues.

--- lib/games_puzzles_algorithms/games/nim/SampleRun.py
+++ lib/games_puzzles_algorithms/games/nim/SampleRun.py
@@ -1,10 +1,13 @@
 import Board
 
+
 b = Board.Board()
 b.printHeap()
-while (not b.isItEnd()):
-    b.userMove()
-    if(b.isItEnd()):
-        break
-    b.computerMove()
 
+while not b.isItEnd():
+    b.userMove()
+
+    if b.isItEnd():
+        break
+
+    b.computerMove()
