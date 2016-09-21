d8448785a9912ebf420c8c99c141fb8cd1206cf3
Noah Weninger <nweninge@ualberta.ca>
2016-09-21 13:54:02 -0600

    update tests

--- lib/test/games/hex/hex_game_state_test.py
+++ lib/test/games/hex/hex_game_state_test.py
@@ -1,7 +1,7 @@
 import pytest
 import random
 from games_puzzles_algorithms.games.hex.game_state import color_to_player
-from games_puzzles_algorithms.games.hex.game_state import COLORS
+from games_puzzles_algorithms.games.hex.game_state import COLOR_NONE, COLOR_BLACK, COLOR_WHITE
 from games_puzzles_algorithms.games.hex.game_state import GameState
 from games_puzzles_algorithms.games.hex.game_state import IllegalAction
 
@@ -19,10 +19,10 @@ def test_one_by_one():
 
 def test_white_win1():
     patient = GameState.root(5)
-    patient.place(patient.board.cell_index(1, 0), COLORS['white'])
-    patient.place(patient.board.cell_index(1, 1), COLORS['white'])
-    patient.place(patient.board.cell_index(1, 2), COLORS['white'])
-    patient.place(patient.board.cell_index(1, 3), COLORS['white'])
+    patient.place(patient.board.cell_index(1, 0), COLOR_WHITE)
+    patient.place(patient.board.cell_index(1, 1), COLOR_WHITE)
+    patient.place(patient.board.cell_index(1, 2), COLOR_WHITE)
+    patient.place(patient.board.cell_index(1, 3), COLOR_WHITE)
 
     assert str(patient) == (
 '''
@@ -35,7 +35,7 @@ def test_white_win1():
         @  @  @  @  @'''
     )
 
-    patient.place(patient.board.cell_index(1, 4), COLORS['white'])
+    patient.place(patient.board.cell_index(1, 4), COLOR_WHITE)
 
     assert str(patient) == (
 '''
@@ -48,15 +48,15 @@ def test_white_win1():
         @  @  @  @  @'''
     )
 
-    assert patient.winner() == COLORS['white']
+    assert patient.winner() == COLOR_WHITE
 
 
 def test_white_win2():
     patient = GameState.root(5)
-    patient.place(patient.board.cell_index(1, 0), COLORS['white'])
-    patient.place(patient.board.cell_index(1, 1), COLORS['white'])
-    patient.place(patient.board.cell_index(1, 2), COLORS['white'])
-    patient.place(patient.board.cell_index(1, 3), COLORS['white'])
+    patient.place(patient.board.cell_index(1, 0), COLOR_WHITE)
+    patient.place(patient.board.cell_index(1, 1), COLOR_WHITE)
+    patient.place(patient.board.cell_index(1, 2), COLOR_WHITE)
+    patient.place(patient.board.cell_index(1, 3), COLOR_WHITE)
 
     assert str(patient) == (
 '''
@@ -69,7 +69,7 @@ def test_white_win2():
         @  @  @  @  @'''
     )
 
-    patient.place(patient.board.cell_index(0, 4), COLORS['white'])
+    patient.place(patient.board.cell_index(0, 4), COLOR_WHITE)
 
     assert str(patient) == (
 '''
@@ -82,13 +82,13 @@ def test_white_win2():
         @  @  @  @  @'''
     )
 
-    assert patient.winner() == COLORS['white']
+    assert patient.winner() == COLOR_WHITE
 
 
 def test_legal_neighbors():
     patient = GameState.root(5)
-    patient.place(patient.board.cell_index(1, 1), COLORS['white'])
-    patient.place(patient.board.cell_index(0, 1), COLORS['black'])
+    patient.place(patient.board.cell_index(1, 1), COLOR_WHITE)
+    patient.place(patient.board.cell_index(0, 1), COLOR_BLACK)
 
     assert str(patient) == (
 '''
@@ -104,7 +104,7 @@ def test_legal_neighbors():
     num_neighbors = 5
     count = 0
     for cell in patient.board.every_legal_neighbor(1, 1):
-        patient.place(patient.board.cell_index(*cell), COLORS['white'])
+        patient.place(patient.board.cell_index(*cell), COLOR_WHITE)
         count += 1
 
     assert count == num_neighbors
@@ -123,7 +123,7 @@ def test_legal_neighbors():
 
 def test_neighbors():
     patient = GameState.root(5)
-    patient.place(patient.board.cell_index(1, 1), COLORS['white'])
+    patient.place(patient.board.cell_index(1, 1), COLOR_WHITE)
 
     assert str(patient) == (
 '''
@@ -139,7 +139,7 @@ def test_neighbors():
     num_neighbors = 6
     count = 0
     for cell in patient.board.every_legal_neighbor(1, 1):
-        patient.place(patient.board.cell_index(*cell), COLORS['white'])
+        patient.place(patient.board.cell_index(*cell), COLOR_WHITE)
         count += 1
 
     assert count == num_neighbors
@@ -158,10 +158,10 @@ def test_neighbors():
 
 def test_set_colorless1():
     patient = GameState.root(5)
-    patient.place(patient.board.cell_index(0, 0), COLORS['white'])
+    patient.place(patient.board.cell_index(0, 0), COLOR_WHITE)
 
     with pytest.raises(IllegalAction):
-        patient.place(patient.board.cell_index(0, 0), COLORS['none'])
+        patient.place(patient.board.cell_index(0, 0), COLOR_NONE)
     assert str(patient) == (
 '''
   A  B  C  D  E
@@ -178,7 +178,7 @@ def test_set_colorless2():
     patient = GameState.root(5)
 
     with pytest.raises(IndexError):
-        patient.place(patient.board.cell_index(0, 0), COLORS['none'])
+        patient.place(patient.board.cell_index(0, 0), COLOR_NONE)
     assert str(patient) == (
 '''
   A  B  C  D  E
@@ -193,9 +193,9 @@ def test_set_colorless2():
 
 def test_with_action_applied():
     patient = GameState.root(5)
-    assert patient.player_to_act() == color_to_player(COLORS['white'])
+    assert patient.player_to_act() == color_to_player(COLOR_WHITE)
     for _ in patient.with_action_applied(0):
-        assert patient.player_to_act() == color_to_player(COLORS['white'])
+        assert patient.player_to_act() == color_to_player(COLOR_WHITE)
         assert str(patient) == (
 '''
   A  B  C  D  E
@@ -206,7 +206,7 @@ def test_with_action_applied():
     5  .  .  .  .  .  O
         @  @  @  @  @'''
         )
-    assert patient.player_to_act() == color_to_player(COLORS['white'])
+    assert patient.player_to_act() == color_to_player(COLOR_WHITE)
     assert str(patient) == (
 '''
   A  B  C  D  E
@@ -221,25 +221,25 @@ def test_with_action_applied():
 
 def test_two_by_two_bug():
     patient = GameState.root(2)
-    patient.place(patient.board.cell_index(0, 0), COLORS['white'])
-    patient.place(patient.board.cell_index(1, 1), COLORS['white'])
-    patient.place(patient.board.cell_index(0, 1), COLORS['black'])
-    patient.place(patient.board.cell_index(1, 0), COLORS['black'])
+    patient.place(patient.board.cell_index(0, 0), COLOR_WHITE)
+    patient.place(patient.board.cell_index(1, 1), COLOR_WHITE)
+    patient.place(patient.board.cell_index(0, 1), COLOR_BLACK)
+    patient.place(patient.board.cell_index(1, 0), COLOR_BLACK)
 
-    assert patient.winner() == COLORS['black']
+    assert patient.winner() == COLOR_BLACK
 
 
 def test_white_moves_are_not_hidden_from_black():
     patient = GameState.root(5)
     cell = (0, 0)
-    patient.place(patient.board.cell_index(*cell), COLORS['white'])
-    patient.set_player_to_act(color_to_player(COLORS['white']))
-    assert patient.player_to_act() == color_to_player(COLORS['white'])
-    assert patient[cell] == COLORS['white']
+    patient.place(patient.board.cell_index(*cell), COLOR_WHITE)
+    patient.set_player_to_act(color_to_player(COLOR_WHITE))
+    assert patient.player_to_act() == color_to_player(COLOR_WHITE)
+    assert patient[cell] == COLOR_WHITE
 
-    patient.set_player_to_act(color_to_player(COLORS['black']))
-    assert patient.player_to_act() == color_to_player(COLORS['black'])
-    assert patient[cell] == COLORS['white']
+    patient.set_player_to_act(color_to_player(COLOR_BLACK))
+    assert patient.player_to_act() == color_to_player(COLOR_BLACK)
+    assert patient[cell] == COLOR_WHITE
 
     assert str(patient) == (
 '''
@@ -256,14 +256,14 @@ def test_white_moves_are_not_hidden_from_black():
 def test_black_moves_are_not_hidden_from_white():
     patient = GameState.root(5)
     cell = (0, 0)
-    patient.place(patient.board.cell_index(*cell), COLORS['black'])
-    patient.set_player_to_act(color_to_player(COLORS['black']))
-    assert patient.player_to_act() == color_to_player(COLORS['black'])
-    assert patient[cell] == COLORS['black']
+    patient.place(patient.board.cell_index(*cell), COLOR_BLACK)
+    patient.set_player_to_act(color_to_player(COLOR_BLACK))
+    assert patient.player_to_act() == color_to_player(COLOR_BLACK)
+    assert patient[cell] == COLOR_BLACK
 
-    patient.set_player_to_act(color_to_player(COLORS['white']))
-    assert patient.player_to_act() == color_to_player(COLORS['white'])
-    assert patient[cell] == COLORS['black']
+    patient.set_player_to_act(color_to_player(COLOR_WHITE))
+    assert patient.player_to_act() == color_to_player(COLOR_WHITE)
+    assert patient[cell] == COLOR_BLACK
 
     assert str(patient) == (
 '''
@@ -279,51 +279,51 @@ def test_black_moves_are_not_hidden_from_white():
 
 def test_terminal_black():
     patient = GameState.root(5)
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(0, 0), COLORS['black'])
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(1, 0), COLORS['black'])
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(2, 0), COLORS['black'])
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(3, 0), COLORS['black'])
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(4, 0), COLORS['black'])
-    assert patient.winner() == COLORS['black']
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(0, 0), COLOR_BLACK)
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(1, 0), COLOR_BLACK)
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(2, 0), COLOR_BLACK)
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(3, 0), COLOR_BLACK)
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(4, 0), COLOR_BLACK)
+    assert patient.winner() == COLOR_BLACK
 
 
 def test_terminal_black():
     patient = GameState.root(5)
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(0, 0), COLORS['black'])
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(1, 0), COLORS['black'])
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(2, 0), COLORS['black'])
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(3, 0), COLORS['black'])
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(4, 0), COLORS['black'])
-    assert patient.winner() == COLORS['black']
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(0, 0), COLOR_BLACK)
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(1, 0), COLOR_BLACK)
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(2, 0), COLOR_BLACK)
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(3, 0), COLOR_BLACK)
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(4, 0), COLOR_BLACK)
+    assert patient.winner() == COLOR_BLACK
 
-    patient.set_player_to_act(color_to_player(COLORS['white']))
+    patient.set_player_to_act(color_to_player(COLOR_WHITE))
     patient.undo()
-    assert patient.winner() == COLORS['none']
+    assert patient.winner() == COLOR_NONE
 
 
 def test_terminal_white():
     patient = GameState.root(5)
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(0, 0), COLORS['white'])
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(0, 1), COLORS['white'])
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(0, 2), COLORS['white'])
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(0, 3), COLORS['white'])
-    assert patient.winner() == COLORS['none']
-    patient.place(patient.board.cell_index(0, 4), COLORS['white'])
-    assert patient.winner() == COLORS['white']
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(0, 0), COLOR_WHITE)
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(0, 1), COLOR_WHITE)
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(0, 2), COLOR_WHITE)
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(0, 3), COLOR_WHITE)
+    assert patient.winner() == COLOR_NONE
+    patient.place(patient.board.cell_index(0, 4), COLOR_WHITE)
+    assert patient.winner() == COLOR_WHITE
 
 
 def test_board_prints():
@@ -380,7 +380,7 @@ def test_undo():
     5  .  .  .  .  .  O
         @  @  @  @  @'''
     )
-    
+
 def test_border_cells():
     patient = GameState.root(5)
     border_cells = patient.board.border_cells(0, -1)
@@ -395,29 +395,29 @@ def test_border_cells():
     border_cells = patient.board.border_cells(1, -2)
     expected_cells = [(0, 4), (1, 4), (2, 4), (3, 4), (4, 4)]
     assert border_cells == expected_cells
-    
+
 def test_connected_neighbors_with_edge():
     patient = GameState.root(5)
     neighbors = []
     for neighbor in patient.board.connected_neighbors(-1, 1):
         neighbors.append(neighbor)
-    
+
     neighbors.sort()
     expected = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)]
     assert expected == neighbors
-    
+
 def test_connected_neighbors_with_border_cell():
     patient = GameState.root(5)
     neighbors = []
     for neighbor in patient.board.connected_neighbors((0, 0), 0):
         neighbors.append(neighbor)
-    
+
     assert -1 in neighbors
     neighbors.remove(-1)
     neighbors.sort()
     expected = [(0, 1), (1, 0)]
     assert expected == neighbors
-    
+
 def test_connected_neighbors_with_played_cell():
     patient = GameState.root(5)
     patient.play(patient.board.cell_index(4, 4))
@@ -425,29 +425,29 @@ def test_connected_neighbors_with_played_cell():
     neighbors = []
     for neighbor in patient.board.connected_neighbors((0, 1), 0):
         neighbors.append(neighbor)
-    
+
     assert -1 in neighbors
     neighbors.remove(-1)
     neighbors.sort()
     expected = [(1, 0), (0, 0), (0, 2), (2, 0), (1, 2), (2, 1)]
     expected.sort()
     assert expected == neighbors
-    
+
 def test_dijkstra_distance():
     patient = GameState.root(5)
     distance = patient.board.dijkstra_distance(0, -1, -2)
     assert distance == 6
-    
+
 def test_dijkstra_distance_with_played_cell():
     patient = GameState.root(5)
     patient.play(patient.board.cell_index(2, 0))
     distance = patient.board.dijkstra_distance(1, -1, -2)
     assert distance == 5
-    
+
 def test_heuristic():
     patient = GameState.root(5)
     assert patient.heuristic(0) == 0
-    
+
 def test_heuristic_with_played_cell():
     patient = GameState.root(5)
     patient.play(patient.board.cell_index(2, 0))
