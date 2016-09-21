78b306aa88343ec6c73e5d770f55e1c9d9f5810d
Noah Weninger <nweninge@ualberta.ca>
2016-09-21 13:50:15 -0600

    10x speedup in some mcts + rave players by not always converting debug messages to json
    also replaced a string hashmap lookup with constants

--- lib/games_puzzles_algorithms/debug.py
+++ lib/games_puzzles_algorithms/debug.py
@@ -5,9 +5,11 @@ import logging
 
 
 def log(data, level=logging.INFO, raw=False):
+    if logging.getLogger().getEffectiveLevel() >= level:
+        return
     sf = traceback.extract_stack()[-2]
     logger = logging.getLogger(sf[0])
-    data_to_print = data if raw else json.dumps(data, sort_keys=True, indent=4)
+    data_to_print = data() if raw else json.dumps(data(), sort_keys=True, indent=4)
     logger.log(level, "\nIn {} on line {} in {}\n{}".format(
         sf[0], sf[1], sf[2], data_to_print)
     )
