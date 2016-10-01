bc0f1cb9c4249f51327412651fe7307614639251
rbh@imac.home <hayward@ualberta.ca>
2016-10-01 16:43:06 -0600

    nim coordinates

--- simple/nim/nim.py
+++ simple/nim/nim.py
@@ -1,6 +1,37 @@
 # play nim RBH 2016
 # under construction
 from sys import stdin
+from time import sleep
+from random import randint
+
+class Nimgame:
+  def psn(self, coord):
+    return sum([a*b for a,b in zip(coord,self.M)])
+
+  def __init__(self):
+    while True:
+      dim = input('nim game pile sizes (eg. 3 5 7)   ')
+      try:
+        self.dim = [ int(x) for x in dim.split() ]
+        if len(self.dim) > 0 and all(d >= 0 for d in self.dim):
+          break
+        else:
+          print('invalid, try again')
+      except ValueError: 
+        print('invalid, try again')
+    print(self.dim)
+    m, self.M = 1, [1]
+    for j in reversed(self.dim):
+      m = m*(j+1)
+      self.M.append(m)
+    self.M.pop()
+    self.M.reverse()
+    print(self.M)
+    rc = [ randint(0,d) for d in self.dim ]
+    print(rc, self.psn(rc), self.psn((0,0,0)), self.psn(self.dim))
+
+def psn(coord, multiplier):
+  return sum([a*b for a,b in zip(coord,multiplier)])
 
 def showboard(L):
   if gameover(L): 
@@ -53,7 +84,5 @@ def playgame():
       else:
         makemove(cmd[0][0], int(cmd[1]), L)
 
-#L = [3,5,7]
-#showboard(L)
-#makemove('b',1,L)
-playgame()
+#playgame()
+g = Nimgame()
