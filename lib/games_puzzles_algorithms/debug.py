c8a21e526f485aa0cabe071f538f8f660e033d6e
treedust <volivare@ualberta.ca>
2016-10-10 13:18:20 -0600

    teaching log mod added

--- lib/games_puzzles_algorithms/debug.py
+++ lib/games_puzzles_algorithms/debug.py
@@ -13,3 +13,13 @@ def log(data, level=logging.INFO, raw=False):
     logger.log(level, "\nIn {} on line {} in {}\n{}".format(
         sf[0], sf[1], sf[2], data_to_print)
     )
+
+def log_t(data, level=logging.INFO, raw=False):
+    if logging.getLogger().getEffectiveLevel() != logging.INFO:
+        return
+    sf = traceback.extract_stack()[-2]
+    logger = logging.getLogger(sf[0])
+    data_to_print = data() if raw else json.dumps(data(), sort_keys=True, indent=4)
+    logger.log(level, "\nIn {} on\nline {} in {}\n{}".format(
+        sf[0], sf[1], sf[2], data_to_print)
+    )
