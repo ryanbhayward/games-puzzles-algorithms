1280977233279377615028003a178ae5dc7a8226
Ryan B Hayward <hayward@ualberta.ca>
2016-10-13 18:08:48 -0600

    migrating to numpy?

--- simple/hex/hex.py
+++ simple/hex/hex.py
@@ -1,27 +1,37 @@
 # simple hex player RBH 2016
-
+import numpy as np
 from random import shuffle
-from sys import stdin
-from copy import deepcopy
+
+# each cell is empty, black, white, or both black and white 
+#   (cells both black and white are dead, and effectively ignored)
+class Cell(): e, b, w, bw  = -1, 0, 1, 2
+
+#bvals = [Cell.e, Cell.b, Cell.w, Cell.bw]
+#B = np.array(bvals,dtype=np.int8)
+#print(B)
+#C = np.array([[Cell.e]*3]*2, dtype = np.int8)
+#print(C)
+#for row in C:
+  #for col in row: print(col, end='')
+  #print('')
 
 class Hexstate:
-#  board is string, padded with 
-#    black at top
-#    white on sides
-#    pad (currently 2) rings of offboard markers
+#  board encircled with rings of offboard markers:
+#    black at top, white on sides
+#  must be at least one ring
 
-#  @@******@@
-#   @@******@@
+#  --******--
+#   --******--
 #    oo......oo
 #     oo......oo
 #      oo......oo
 #       oo......oo
 #        oo......oo
 #         oo......oo
-#          @@******@@
-#           @@******@@
+#          --******--
+#           --******--
   def psn(self, row, col):
-    return (self.pad + row)*(2*self.pad + self.c) + self.pad + col
+    return (self.rings + row)*(2*self.rings + self.c) + self.rings + col
 
   def gameover(self):
     return False
@@ -31,32 +41,32 @@ class Hexstate:
     self.brd = self.brd[0:p] + ch + self.brd[p+1:]
 
   def __init__(self, rows, cols):
-    self.pad = 1 # number of padding rings around board
-    assert(self.pad > 0)
-    self.r, self.c, self.bsize =  rows, cols, (2*self.pad+rows)*(2*self.pad+cols)
+    self.rings = 1 # number of ringsding rings around board
+    assert(self.rings > 0)
+    self.r, self.c, self.bsize =  rows, cols, (2*self.rings+rows)*(2*self.rings+cols)
     # cell can be black, white, blackwhite (both), empty
-    self.chBW, self.chB, self.chW, self.chE = '-', '*', 'o', '.'
+    self.chBW, self.chB, self.chW, self.chE = '-', '*', '@', '.'
     self.brd = ''
-    # top rows (padding)
-    self.brd += (self.chBW * self.pad + self.c * self.chB + self.chBW * self.pad) * self.pad
+    # top rows (ringsding)
+    self.brd += (self.chBW * self.rings + self.c * self.chB + self.chBW * self.rings) * self.rings
     # next rows 
-    self.brd += (self.chW  * self.pad + self.c * self.chE + self.chW  * self.pad) * self.r
-    # bottom rows (padding)
-    self.brd += (self.chBW * self.pad + self.c * self.chB + self.chBW * self.pad) * self.pad
+    self.brd += (self.chW  * self.rings + self.c * self.chE + self.chW  * self.rings) * self.r
+    # bottom rows (ringsding)
+    self.brd += (self.chBW * self.rings + self.c * self.chB + self.chBW * self.rings) * self.rings
 
   def showboard(self):
     rowlabelfield = '  '
     if self.r > 9: rowlabelfield += ' '
     pretty, row = '\n' + rowlabelfield, 0
-    for x in range(self.pad - 1): pretty += '  '
+    for x in range(self.rings - 1): pretty += '  '
     for c in range(self.c): pretty += ' ' + chr(ord('a')+c)
     pretty += '\n' + rowlabelfield
     for j in range(self.bsize):
       pretty += self.brd[j] + ' '
-      if j%(2*self.pad + self.c) == (2*self.pad - 1) + self.c: 
+      if j%(2*self.rings + self.c) == (2*self.rings - 1) + self.c: 
         row += 1
         pretty += '\n' + ' '*row
-        rowonboard = 1 + row - self.pad
+        rowonboard = 1 + row - self.rings
         if rowonboard < 1 or rowonboard > self.r:
           pretty += rowlabelfield
         if rowonboard > 0 and rowonboard <= self.r:
@@ -69,7 +79,7 @@ class Hexstate:
   def genmove(self, cmd):
     assert(cmd[0][0]=='g')
     cmd = cmd.split()
-    if len(cmd)==2 and (cmd[1][0]=='b' or cmd[1][0]=='w'):
+    if len(cmd)==2 and (cmd[1][0]==self.chB or cmd[1][0]==self.chW):
       print(' genmove coming soon')
     else:
       print(' invalid genmove request')
