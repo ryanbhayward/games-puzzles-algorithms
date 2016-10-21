ceae48fbae8dab97a2ca442a085fa1d783ac95d4
Ryan B Hayward <hayward@ualberta.ca>
2016-10-20 18:16:02 -0600

    genmove working now

--- simple/ttt/ttt_classic.py
+++ simple/ttt/ttt_classic.py
@@ -1,21 +1,25 @@
-# classic ttt: 3x3 board, solve with alphabetanega     RBH 2016
-# to make alphabeta faster:
+# classic ttt: 3x3 board          RBH 2016
+# - genmove finds value of all moves, using alphabeta search
+
+# implemented this alphabeta improvement:
 #  - instead of searching over all children of a node,
-#  - search over all non-isomorphic children,
-#  - since any two isomorphic children will have the same value
-#  - the symmetry group of the board has 8 elements,
-#  - if any two of those map to the same position, then
-#  - those two positions are isomorphic
+#  - search only over non-isomorphic children
+#  - (any two isomorphic children will have the same value)
+#  - the symmetry group (rotate/flip) of the board has 8 elements:
+#  -   if any two symmetries operations yield the same position, then
+#  -   the two corresponding positions are isomorphic
 
 import numpy as np
 from time import sleep
 
 class Cell: # each cell is one of these: empty, x, o
   n,e,x,o,chars = 9,0,1,2,'.xo' 
+def opponent(c): return 3-c
 
-# each board position 0,1,2, so we can think of a board
-#   position as a 9-digit number base 3
-# so number of states is same as max 9-digit base_3 integer
+# each cell is 0,1,2
+# so number positions == 3**9
+# can represent position as 9-digit base_3 number
+
 ttt_states = 19683  # 3**Cell.n
 powers_of_3 = np.array( # for converting position to base_3 int
   [1, 3, 9, 27, 81, 243, 729, 2187, 6561], dtype=np.int16)
@@ -25,13 +29,13 @@ def board_to_int(B):
 
 # consider all possible symmetric positions, return min
 def hash(L): # using numpy array indexing here
-  return min([board_to_int( L[Syms[j]] ) for j in range(8)])
+  return min([board_to_int( L[Isos[j]] ) for j in range(8)])
 
 # convert from integer for board position
 def base_3( y ): 
   assert(y <= ttt_states)
   L = [0]*Cell.n
-  for j in range(9):
+  for j in range(Cell.n):
     y, L[j] = divmod(y,3)
     if y==0: break
   return np.array( L, dtype = np.int16)
@@ -58,8 +62,8 @@ def genmoverequest(cmd):
   return invalid
 
 def printmenu():
-  print('  x b2         play X b 2')
-  print('  o e3         play O e 3')
+  print('  x b2         play x b 2')
+  print('  o e3         play o e 3')
   print('  . a2          erase a 2')
   print('  u                  undo')
   print('  ?           solve state')
@@ -84,47 +88,39 @@ def showboard(psn):
   for j in range(3): # rows
     pretty += ' ' + paint(str(1+j)) + ' '
     for k in range(3): # columns
-      pretty += ' ' + paint(Cell.chars[psn.brd[rc_to_psn(j,k)]])
+      pretty += ' ' + paint(Cell.chars[psn.brd[rc_to_lcn(j,k)]])
     pretty += '\n'
   print(pretty)
 
-### position tuples of 8 symmetric ttt board permutations
-Syms = np.array(( (0,1,2,3,4,5,6,7,8),
+### permutations showing possible board isomorphisms
+Isos = np.array(( (0,1,2,3,4,5,6,7,8),
          (0,3,6,1,4,7,2,5,8),
          (2,1,0,5,4,3,8,7,6),
          (2,5,8,1,4,7,0,3,6),
          (8,7,6,5,4,3,2,1,0),
-         (8,5,2,7,1,4,6,3,0),
+         (8,5,2,7,4,1,6,3,0),
          (6,7,8,3,4,5,0,1,2),
          (6,3,0,7,4,1,8,5,2)
          ), dtype = np.int8)
 
-Win_lines = np.array(( # psn tuples of 8 winning lines
+Win_lines = np.array(( # 8 winning lines, as location triples
   (0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)
   ), dtype=np.int8)
 
-# board positions
+##################### board state ########################
+# cell indices, or locations:
 #   0 1 2
 #   3 4 5
 #   6 7 8
-def rc_to_psn(r,c): return r*3 + c
+
+def rc_to_lcn(r,c): 
+  return r*3 + c
+
+def lcn_to_alphanum(p):
+  r, c = divmod(p,3)
+  return 'abc'[c] + '123'[r]
 
 class Position: # ttt board with x,o,e cells
-  def solve(self):
-    self.static_win = np.array( [0]*ttt_states, dtype = np.int8)
-    tmp = self.brd
-    for j in range(len(self.static_win)):
-      self.brd = base_3(j)
-      bitset = 0               
-      if self.has_win(Cell.x): bitset += 2
-      if self.has_win(Cell.o): bitset += 1
-      self.static_win[j] = bitset
-      # bitset 3: both    win
-      # bitset 2: x only  win
-      # bitset 1: o only  win
-      # bitset 0: neither win
-    self.brd = tmp
-    
   def legal_moves(self):
     L = []
     for j in range(Cell.n):
@@ -132,18 +128,16 @@ class Position: # ttt board with x,o,e cells
         L.append(j)
     return L
 
-  def asym_moves(self, L, cell):
+  def non_iso_moves(self, L, cell): # number of non-isomorphic moves
     assert(len(L)>0)
     H, X = [], []
     for j in range(len(L)):
       p = L[j]
-      #print('move to cell',p,end='')
       self.brd[p] = cell
       h = hash(self.brd)
       if h not in H:
         H.append(h)
         X.append(j)
-      #print(h, H, X)
       self.brd[p] = Cell.e
     L = np.array(L)
     X = np.array(X)
@@ -167,11 +161,10 @@ class Position: # ttt board with x,o,e cells
     return False
 
   def putstone(self, row, col, color):
-    self.brd[rc_to_psn(row,col)] = color
+    self.brd[rc_to_lcn(row,col)] = color
 
   def __init__(self, y):
     self.brd = base_3(y)
-    self.static_win = None
 
   def genmove(self, request):
     if request[0]:
@@ -179,8 +172,16 @@ class Position: # ttt board with x,o,e cells
       if len(L)==0:
         print('board full, no move possible')
       else:
-        cell = request[1]
-        if self.haswin(cell) or self.haswin(3-cell):
+        ptm = char_to_cell(request[1])
+        if self.has_win(ptm) or self.has_win(opponent(ptm)):
+          print('board already has winning line(s)')
+        else:
+          A = self.non_iso_moves(L,ptm)
+          for cell in A:
+            self.brd[cell] = ptm
+            print(' ',Cell.chars[ptm],'plays',lcn_to_alphanum(cell), 
+              'result:',-alphabetanega(0, self, opponent(ptm), -1, 1))
+            self.brd[cell] = Cell.e   
     else:
       print(request[2])
 
@@ -194,30 +195,28 @@ class Position: # ttt board with x,o,e cells
           x, y = int(n) - 1, ord(q)-ord('a')
           if x>=0 and x < 3 and y>=0 and y < 3:
             self.putstone(x, y, char_to_cell(ch))
-            H.append(3*x+y) # add position to history
+            H.append(rc_to_lcn(x,y)) # add location to history
             return
           else: print('\n  coordinate off board')
     print('  ... ? ... sorry ...\n')
 
-def undo(H, brd):
+def undo(H, brd):  # pop last location, erase that cell
   if len(H)==0:
     print('\n    board empty, nothing to undo\n')
   else:
-    psn = H.pop()
-    brd[psn] = Cell.e
+    lcn = H.pop()
+    brd[lcn] = Cell.e
 
 ####################### search
 def alphabetanega(d, psn, ptm, alpha, beta): # ptm: 1/0/-1 win/draw/loss
-  #print(' '*d, Cell.chars[ptm], alpha, beta)
-  #showboard(psn)
   if psn.has_win(ptm):     return 1  # previous move created win
   L = psn.legal_moves()
   if len(L) == 0:          return 0  # board full, no winner
-  A = psn.asym_moves(L,ptm)
+  A = psn.non_iso_moves(L,ptm)
   so_far = -1  # best score so far
   for cell in A:
     psn.brd[cell] = ptm
-    so_far = max(so_far,-alphabetanega(d+1, psn, 3-ptm,-beta, -alpha))
+    so_far = max(so_far,-alphabetanega(d+1, psn, opponent(ptm),-beta, -alpha))
     psn.brd[cell] = Cell.e   # reset brd to original
     alpha = max(alpha, so_far)
     if alpha >= beta:
@@ -227,7 +226,8 @@ def alphabetanega(d, psn, ptm, alpha, beta): # ptm: 1/0/-1 win/draw/loss
 def info(p):
     h, L = hash(p.brd), p.legal_moves()
     print('  hash', h, '\n  legal moves', L)
-    print('  asym moves x o', p.asym_moves(L,Cell.x), p.asym_moves(L,Cell.o))
+    print('  non-isomorphic moves x o', p.non_iso_moves(L,Cell.x), 
+                              p.non_iso_moves(L,Cell.o))
     for cell in (Cell.x, Cell.o):
       print('  ',Cell.chars[cell], 'alphabeta score:',end='')
       print('  ',alphabetanega(0, p, cell, -1, 1))
