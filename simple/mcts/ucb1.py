afa8010d011e76fe0b326f81c4168f07c2bc2967
Ryan B Hayward <hayward@ualberta.ca>
2016-11-15 13:46:35 -0700

    more info in output

--- simple/mcts/ucb1.py
+++ simple/mcts/ucb1.py
@@ -1,8 +1,9 @@
 # implement ucb1 formula
 import math
 
+T = 5.0
 def win_rate(w,v):
-  return (w + 5.0) / (v + 10.0)
+  return (w + T) / (v + T + T)
 
 def ucb(c,w,v,V):
   return win_rate(w,v) + c* math.sqrt(math.log(V)/v)
@@ -14,17 +15,19 @@ def win_rate_test(n):
     print('')
 
 def ucb_demo(n):
-  c, v = .1, 1
-  print('constant c', c)
-  print('w v V ratios  0 1 2  .4 1 2  10 20 40  50 100 200') 
+  c, v = .5, 1
+  print('UCB1 formula values')
+  print('win rate additive factor T', T)
+  print('ucb multiplicative constant c', c)
+  print('w v V ratios  0 1 2  1 3 6  9 20 40  50 100 200') 
   for k in range(n):
-    print('v', '{:3d}'.format(v), ' ', end='')
+    print('v', '{:4d}'.format(v), ' ', end='')
     print(' ', 
       '{0:.3f}'.format(ucb(c,0,v,v+v)).lstrip('0'), 
-      '{:.3f}'.format(ucb(c,.4*v,v,v+v)).lstrip('0'), 
-      '{:.3f}'.format(ucb(c,10*v, 20*v, 40*v)).lstrip('0'), 
+      '{:.3f}'.format(ucb(c,v,2*v,6*v)).lstrip('0'), 
+      '{:.3f}'.format(ucb(c,9*v, 20*v, 40*v)).lstrip('0'), 
       '{:.3f}'.format(ucb(c,50*v, 100*v, 200*v)).lstrip('0'))
     v *= 2
 
-win_rate_test(8)
-ucb_demo(10)
+#win_rate_test(8)
+ucb_demo(12)
