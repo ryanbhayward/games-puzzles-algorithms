d21cd09d2aba5c132b5f07244d1f109810e0246c
Ryan Hayward <hayward@ualberta.ca>
2018-10-11 06:59:47 -0600

    nodes in ttt search space

---
+++ simple/ttt/node-count.py
@@ -0,0 +1,19 @@
+# upper bound on number of nodes in ttt search tree
+
+def fallfac(n,k):
+  p = 1
+  for j in range(k):
+    p *= n-j
+  return p
+
+def nodes():
+  s = 1
+  for j in range(8):
+    s +=  fallfac(9, j+1)
+  return s
+  
+
+for j in range(8):
+  print(j, fallfac(9,j+1))
+
+print(nodes())
