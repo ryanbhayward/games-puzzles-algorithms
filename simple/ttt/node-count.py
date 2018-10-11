f40448550b7cc06f22a72776519c97d164adf6f4
Ryan Hayward <hayward@ualberta.ca>
2018-10-11 07:53:45 -0600

    fixed node-count

--- simple/ttt/node-count.py
+++ simple/ttt/node-count.py
@@ -8,12 +8,11 @@ def fallfac(n,k):
 
 def nodes():
   s = 1
-  for j in range(8):
+  for j in range(9):
     s +=  fallfac(9, j+1)
   return s
   
-
-for j in range(8):
+for j in range(9):
   print(j, fallfac(9,j+1))
 
 print(nodes())
