683617bd54eb4c7147ae54ae0892c2d6c56f281b
Ryan Hayward <hayward@ualberta.ca>
2019-10-18 14:20:36 -0600

    working

--- simple/nim/nimstates.py
+++ simple/nim/nimstates.py
@@ -1,3 +1,15 @@
+# using recursion, compute the number of nodes in the nim state space
+#   in three different cases        RBH 2019
+# 1)  search space is a tree, no check for isomorphism,
+#       e.g. six permutations of (1 2 3) are considered different
+# 2)  search space is a tree, but check for isomorphism,
+#       e.g. six permutations (1 2 3) are considered the same
+#       ... this reduces the number of children of a node
+# 3)  search space is a dag... so the number of nodes
+#       in the search space is just the number of
+#       positions reachable from the original ...
+#       there will be no duplicate nodes because of transposition
+
 def get_piles():
    while True:
        raw = input('nim game pile sizes (eg. 3 5 7)   ')
@@ -42,9 +54,25 @@ def tree_size(nim_psn, sd, srt):  # tuple, dictionary, boolean
   sd.update({ psn: ts })
   return ts
 
-def dag_size(
+def dag_nodes(nim_psn,S): # return set of nodes in DAG
+  S.add(nim_psn)
+  if all(p == 0 for p in nim_psn):
+    return S
+  psn = tuple(sorted(nim_psn))
+  for j in range(len(psn)):
+    for k in range(psn[j]):
+      new = psn[:j] + (k,) + psn[j+1:]
+      new = tuple(sorted(new))
+      if new not in S: 
+        S = dag_nodes(new, S)
+  return S
 
 v = get_piles()
 for b in [False, True]:
   TS = dict() # tree sizes
   print(tree_size(v, TS, b))
+S = set()
+S = dag_nodes(v, S)
+print(len(S))
+#for j in S:
+#  print(j)
