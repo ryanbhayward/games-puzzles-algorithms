f84c6822aa795c27523ff33d50589f57e88f3170
silvavn <37382997+silvavn@users.noreply.github.com>
2020-09-23 11:55:15 -0600

    Fixes windows run
    Allows windows users to run
    `python life.py filename`

--- simple/life/life-asn1.py
+++ simple/life/life-asn1.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+from paint import paint
+import sys
+from time import sleep
+import numpy as np
 """
 # Sample format to answer pattern questions 
 # assuming the pattern would be frag0:
@@ -36,14 +40,11 @@ Sarah Levy                    sarlevy
 based on life-np.py from course repo
 """
 
-import numpy as np
-from time import sleep
-from sys import stdin
-from paint import paint
 
 PTS = '.*#'
 DEAD, ALIVE, WALL = 0, 1, 2
 DCH, ACH, GCH = PTS[DEAD], PTS[ALIVE], PTS[WALL]
+
 
 def point(r, c, cols): return c + r*cols
 
@@ -52,75 +53,96 @@ board functions
   * represent board as 2-dimensional array
 """
 
-def get_board(): 
-  B = []
-  for line in stdin:
-    B.append(line.rstrip().replace(' ',''))
-  rows, cols = len(B), len(B[0])
-  for j in range(1, rows): assert(len(B[j]) == cols)
-  return B, rows, cols
 
-def convert_board(B, r, c): # from string to numpy array
-  A = np.zeros((r,c), dtype=np.int8)
-  for j in range(r):
-    for k in range(c):
-      if B[j][k]==ACH: A[j,k] = ALIVE
-  return A
+def get_board():
+    B = []
+    print(sys.argv[1])
+    with open(sys.argv[1]) as f:
+        for line in f:
+            B.append(line.rstrip().replace(' ', ''))
+        rows, cols = len(B), len(B[0])
+        for j in range(1, rows):
+            assert(len(B[j]) == cols)
+        return B, rows, cols
 
-def expand_grid(A, r, c, t): # add t empty rows and columns on each side
-  N = np.zeros((r+2*t,c+2*t), dtype=np.int8)
-  for j in range(r):
-    for k in range(c):
-      if A[j][k]==ALIVE: N[j+t,k+t] = ALIVE
-  return N, r+2*t, c+2*t
 
-def print_array(A, r, c): 
-  print('')
-  for j in range(r):
-    out = ''
-    for k in range(c):
-      out += ACH if A[j,k]==ALIVE else DCH
-    print(out)
+def convert_board(B, r, c):  # from string to numpy array
+    A = np.zeros((r, c), dtype=np.int8)
+    for j in range(r):
+        for k in range(c):
+            if B[j][k] == ACH:
+                A[j, k] = ALIVE
+    return A
+
+
+def expand_grid(A, r, c, t):  # add t empty rows and columns on each side
+    N = np.zeros((r+2*t, c+2*t), dtype=np.int8)
+    for j in range(r):
+        for k in range(c):
+            if A[j][k] == ALIVE:
+                N[j+t, k+t] = ALIVE
+    return N, r+2*t, c+2*t
+
+
+def print_array(A, r, c):
+    print('')
+    for j in range(r):
+        out = ''
+        for k in range(c):
+            out += ACH if A[j, k] == ALIVE else DCH
+        print(out)
+
 
 def show_array(A, r, c):
-  for j in range(r):
-    line = ''
-    for k in range(c):
-      line += str(A[j,k])
-    print(line)
-  print('')
+    for j in range(r):
+        line = ''
+        for k in range(c):
+            line += str(A[j, k])
+        print(line)
+    print('')
+
 
 """ 
 Conway's next-state formula
 """
 
+
 def next_state(A, r, c):
-  N = np.zeros((r,c), dtype=np.int8)
-  changed = False
-  for j in range(r):
-    for k in range(c):
-      num = 0
-      if j>0   and k>0   and A[j-1, k-1] == ALIVE: num += 1
-      if j>0             and A[j-1, k  ] == ALIVE: num += 1
-      if j>0   and k<c-1 and A[j-1, k+1] == ALIVE: num += 1
-      if           k>0   and A[j  , k-1] == ALIVE: num += 1
-      if j>0   and k<c-1 and A[j  , k+1] == ALIVE: num += 1
-      if j<r-1 and k>0   and A[j+1, k-1] == ALIVE: num += 1
-      if j<r-1           and A[j+1, k  ] == ALIVE: num += 1
-      if j<r-1 and k<c-1 and A[j+1, k+1] == ALIVE: num += 1
-      if A[j,k] == ALIVE: 
-        if num > 1 and num < 4: 
-          N[j,k] = ALIVE
-        else:
-          N[j,k] = DEAD 
-          changed = True
-      else:               
-        if num == 3:
-          N[j,k] = ALIVE
-          changed = True
-        else:
-          N[j,k] = DEAD
-  return N, changed
+    N = np.zeros((r, c), dtype=np.int8)
+    changed = False
+    for j in range(r):
+        for k in range(c):
+            num = 0
+            if j > 0 and k > 0 and A[j-1, k-1] == ALIVE:
+                num += 1
+            if j > 0 and A[j-1, k] == ALIVE:
+                num += 1
+            if j > 0 and k < c-1 and A[j-1, k+1] == ALIVE:
+                num += 1
+            if k > 0 and A[j, k-1] == ALIVE:
+                num += 1
+            if j > 0 and k < c-1 and A[j, k+1] == ALIVE:
+                num += 1
+            if j < r-1 and k > 0 and A[j+1, k-1] == ALIVE:
+                num += 1
+            if j < r-1 and A[j+1, k] == ALIVE:
+                num += 1
+            if j < r-1 and k < c-1 and A[j+1, k+1] == ALIVE:
+                num += 1
+            if A[j, k] == ALIVE:
+                if num > 1 and num < 4:
+                    N[j, k] = ALIVE
+                else:
+                    N[j, k] = DEAD
+                    changed = True
+            else:
+                if num == 3:
+                    N[j, k] = ALIVE
+                    changed = True
+                else:
+                    N[j, k] = DEAD
+    return N, changed
+
 
 #############################################
 """ 
@@ -129,10 +151,13 @@ next_state2 that (for the usual bounded
 rectangular grid) calls the function num_nbrs2,
 and delete the raise error statement:
 """
+
+
 def next_state2():
 
-  raise NotImplementedError()
+    raise NotImplementedError()
 #############################################
+
 
 #############################################
 """ 
@@ -140,11 +165,13 @@ Provide your code for the function
 num_nbrs2 here and delete the raise error
 statement:
 """
+
+
 def num_nbrs2():
 
-
-  raise NotImplementedError()
+    raise NotImplementedError()
 #############################################
+
 
 #############################################
 """ 
@@ -152,10 +179,13 @@ Provide your code for the function
 next_state_torus here and delete the raise 
 error statement:
 """
+
+
 def next_state_torus():
 
-  raise NotImplementedError()
+    raise NotImplementedError()
 #############################################
+
 
 #############################################
 """ 
@@ -163,11 +193,13 @@ Provide your code for the function
 num_nbrs_torus here and delete the raise 
 error statement:
 """
+
+
 def num_nbrs_torus():
 
-
-  raise NotImplementedError()
+    raise NotImplementedError()
 #############################################
+
 
 """
 input, output
@@ -180,20 +212,29 @@ pause = 0.2
 Modify interact as necessary to run the code:
 """
 #############################################
-def interact(max_itn):
-  itn = 0
-  B, r, c = get_board()
-  print(B)
-  X = convert_board(B,r,c)
-  A,r,c = expand_grid(X,r,c,0)
-  print_array(A,r,c)
-  while itn <= max_itn:
-    sleep(pause)
-    newA, delta = next_state(A, r, c)
-    if not delta:  break
-    itn += 1
-    A = newA
-    print_array(A, r, c)
-  print('\niterations', itn)
 
-interact(99)
+
+def interact(max_itn):
+    itn = 0
+    B, r, c = get_board()
+    print(B)
+    X = convert_board(B, r, c)
+    A, r, c = expand_grid(X, r, c, 0)
+    print_array(A, r, c)
+    while itn <= max_itn:
+        sleep(pause)
+        newA, delta = next_state(A, r, c)
+        if not delta:
+            break
+        itn += 1
+        A = newA
+        print_array(A, r, c)
+    print('\niterations', itn)
+
+
+def main():
+    interact(99)
+
+
+if __name__ == '__main__':
+    main()
--- simple/life/life.py
+++ simple/life/life.py
@@ -5,18 +5,23 @@ Conway's game of life, infinite grid    RBH 2020
 """
 
 from time import sleep
-from sys import stdin
+import sys
 from paint import paint
 
 PTS = '.*#'
 DEAD, ALIVE, GUARD = 0, 1, 2
 DCH, ACH, GCH = PTS[DEAD], PTS[ALIVE], PTS[GUARD]
 
+pause = .2
+
 """
 replace char-in-string
 """
 
-def change_str(s, where, what): return s[:where] + what + s[where+1:]
+
+def change_str(s, where, what):
+    return s[:where] + what + s[where+1:]
+
 
 """
 board functions
@@ -25,137 +30,195 @@ board functions
   * cell's coordinate is 2-d row/column indices
 """
 
-def point(r, c, cols): return c + r*cols
 
-def coord(p, cols): return divmod(p, cols)
+def point(r, c, cols):
+    return c + r*cols
 
-def live_row(r, B, cols): return ACH in B[point(r, 1, cols) : point(r, cols, cols)]
+
+def coord(p, cols):
+    return divmod(p, cols)
+
+
+def live_row(r, B, cols):
+    return ACH in B[point(r, 1, cols): point(r, cols, cols)]
+
 
 def live_col(c, B, cols):
-  n, pt = len(B), point(1, c, cols)
-  while pt < n:
-    if B[pt] == ACH: return True
-    pt += cols
-  return False
+    n, pt = len(B), point(1, c, cols)
+    while pt < n:
+        if B[pt] == ACH:
+            return True
+        pt += cols
+    return False
 
-def alphanum(p, C): #for showing coordinates
-  r, c = coord(p, C)
-  return 'abcdefghj'[c] + '1234566789'[r]
 
-def get_board(): 
-  B = []
-  for line in stdin:
-    B.append(line.rstrip().replace(' ',''))
-  rows, cols = len(B), len(B[0])
-  for j in range(1, rows): assert(len(B[j]) == cols)
-  return B, rows, cols
+def alphanum(p, C):  # for showing coordinates
+    r, c = coord(p, C)
+    return 'abcdefghj'[c] + '1234566789'[r]
 
-# to avoid boundary collisions,
-#   ensure B has empty first/last row/column 
-#   by if necessary adding empty first/last row/column
-def pad(B, r, c): 
-  assert(len(B) == r*c+1) # this board is already padded
-  if live_row(r-2, B, c): # last non-guard row
-    B = B[0 : point(r-1, 1, c)] + DCH*(c-1) + GCH*(c+1)
-    r += 1
-  if live_row(1, B, c): # first non-guard row
-    B = GCH*c + GCH + DCH*(c-1) + B[point(1, 0, c) : 1+r*c]
-    r += 1
-  if live_col(c-1, B, c): # last column
-    new = GCH*(c+1)
-    for j in range(1, r-1): new += B[point(j, 0, c) : point(j, c, c)] + DCH
-    B = new + GCH*(c+2)
-    c += 1
-  if live_col(1, B, c): # first non-guard column
-    new = GCH*(c+1)
-    for j in range(1, r-1): new += GCH + DCH + B[point(j, 1, c) : point(j, c, c)]
-    B = new + GCH*(c+2)
-    c += 1
-  return B, r, c
 
-# add guards: top row, left column, bottom row (with one extra)
-# original board        guarded board
-#                          GGGG
-#   .x.                    G.x.
-#   ..x                    G..x
-#   xxx                    Gxxx
-#   ...                    G...
-#                          GGGGG  <- don't forget last guard
+def get_board():
+    B = []
+    print(sys.argv[1])
+    with open(sys.argv[1]) as f:
+        for line in f:
+            B.append(line.rstrip().replace(' ', ''))
+        rows, cols = len(B), len(B[0])
+        for j in range(1, rows):
+            assert(len(B[j]) == cols)
+        return B, rows, cols
+
+
+def pad(B, r, c):
+    '''
+
+    to avoid boundary collisions,
+    ensure B has empty first/last row/column
+    by if necessary adding empty first/last row/column
+    :param B: [description]
+    :type B: [type]
+    :param r: [description]
+    :type r: [type]
+    :param c: [description]
+    :type c: [type]
+    :returns: [description]
+    :rtype: {[type]}
+    '''
+    assert(len(B) == r*c+1)  # this board is already padded
+    if live_row(r-2, B, c):  # last non-guard row
+        B = B[0: point(r-1, 1, c)] + DCH*(c-1) + GCH*(c+1)
+        r += 1
+    if live_row(1, B, c):  # first non-guard row
+        B = GCH*c + GCH + DCH*(c-1) + B[point(1, 0, c): 1+r*c]
+        r += 1
+    if live_col(c-1, B, c):  # last column
+        new = GCH*(c+1)
+        for j in range(1, r-1):
+            new += B[point(j, 0, c): point(j, c, c)] + DCH
+        B = new + GCH*(c+2)
+        c += 1
+    if live_col(1, B, c):  # first non-guard column
+        new = GCH*(c+1)
+        for j in range(1, r-1):
+            new += GCH + DCH + B[point(j, 1, c): point(j, c, c)]
+        B = new + GCH*(c+2)
+        c += 1
+    return B, r, c
+
+
 def add_guards(B, r, c):
-  B.insert(0, GCH * (1 + c))
-  for j in range(r): B[j+1] = GCH + B[j+1]
-  B.append(GCH * (2 + c))
-  return(''.join(B), len(B), len(B[0]))
+    '''
+    add guards: top row, left column, bottom row (with one extra)
+    original board        guarded board
+                             GGGG
+      .x.                    G.x.
+      ..x                    G..x
+      xxx                    Gxxx
+      ...                    G...
+                             GGGGG  <- don't forget last guard
+    [description]
+    :param B: [description]
+    :type B: [type]
+    :param r: [description]
+    :type r: [type]
+    :param c: [description]
+    :type c: [type]
+    '''
+    B.insert(0, GCH * (1 + c))
+    for j in range(r):
+        B[j+1] = GCH + B[j+1]
+    B.append(GCH * (2 + c))
+    return(''.join(B), len(B), len(B[0]))
 
-# add numeric row indices, alphabetic column indices
-def showboard(brd, R, C, gap, pause): 
-  pretty = '\n    ' 
-  if C <= 26:
-    for c in range(C): # columns
-      pretty += gap + paint(chr(ord('a')+c), PTS)
-  else:
-    gap = ''
-    
-  pretty += '\n'
-  for j in range(R): # rows
-    if j < 10: pretty += ' '
-    pretty += gap + paint(str(j), PTS) + ' '
-    for k in range(C): # columns
-      pretty += gap + paint([brd[point(j, k, C)]], PTS)
-    if j == R-1: # in last row, put last guard
-      pretty += gap + paint(brd[R*C], PTS)
+
+def showboard(board, row, C, gap, pause):
+    # add numeric row indices, alphabetic column indices
+    pretty = '\n    '
+    if C <= 26:
+        for c in range(C):  # columns
+            pretty += gap + paint(chr(ord('a')+c), PTS)
+    else:
+        gap = ''
+
     pretty += '\n'
-  print(pretty)
-  sleep(pause)
+    for j in range(row):  # rows
+        if j < 10:
+            pretty += ' '
+        pretty += gap + paint(str(j), PTS) + ' '
+        for k in range(C):  # columns
+            pretty += gap + paint([board[point(j, k, C)]], PTS)
+        if j == row-1:  # in last row, put last guard
+            pretty += gap + paint(board[row*C], PTS)
+        pretty += '\n'
+    print(pretty)
+    sleep(pause)
+
 
 """ 
 Conway's next-state formula
 """
 
-def num_nbrs(s, j, cols, ch): # state, cell, columns, nbr-type
-  num = 0
-  if s[j-(cols+1)] == ch: num += 1
-  if s[j- cols   ] == ch: num += 1
-  if s[j-(cols-1)] == ch: num += 1
-  if s[j-1       ] == ch: num += 1
-  if s[j+1       ] == ch: num += 1
-  if s[j+(cols-1)] == ch: num += 1
-  if s[j+ cols   ] == ch: num += 1
-  if s[j+ cols+1 ] == ch: num += 1
-  return num
+
+def num_nbrs(s, j, cols, ch):
+    # state, cell, columns, nbr-type
+    num = 0
+    if s[j-(cols+1)] == ch:
+        num += 1
+    if s[j - cols] == ch:
+        num += 1
+    if s[j-(cols-1)] == ch:
+        num += 1
+    if s[j-1] == ch:
+        num += 1
+    if s[j+1] == ch:
+        num += 1
+    if s[j+(cols-1)] == ch:
+        num += 1
+    if s[j + cols] == ch:
+        num += 1
+    if s[j + cols+1] == ch:
+        num += 1
+    return num
+
 
 def next_state(s, cols):
-  new = ''
-  for j in range(len(s)):
-    ch = s[j]
-    if   ch == GCH: new += GCH
-    else:
-      m = num_nbrs(s, j, cols, ACH)
-      if ch == ACH: new += ACH if m > 1 and m < 4 else DCH
-      else:         new += ACH if m ==3           else DCH
-  return new
+    new = ''
+    for j in range(len(s)):
+        ch = s[j]
+        if ch == GCH:
+            new += GCH
+        else:
+            m = num_nbrs(s, j, cols, ACH)
+            if ch == ACH:
+                new += ACH if m > 1 and m < 4 else DCH
+            else:
+                new += ACH if m == 3 else DCH
+    return new
 
-"""
-input, output
-"""
-
-pause = .2
 
 def interact(max_itn):
-  itn = 0
-  B, r, c = get_board()
-  B, r, c = add_guards(B, r, c)
-  #showboard(B, r, c, ' ', pause)
-  while itn <= max_itn:
-    #print('iteration', itn)
-    B, r, c = pad(B, r, c)
-    newB = next_state(B, c)
-    if newB == B: break
-    if r <= 10000: 
-      showboard(B, r, c, ' ', pause)
-    itn += 1
-    B = newB
-  showboard(B, r, c, ' ', pause)
-  print('iterations', itn)
+    """
+    input, output
+    """
+    itn = 0
+    B, r, c = get_board()
+    B, r, c = add_guards(B, r, c)
+    while itn <= max_itn:
+        B, r, c = pad(B, r, c)
+        newB = next_state(B, c)
+        if newB == B:
+            break
+        if r <= 10000:
+            showboard(B, r, c, ' ', pause)
+        itn += 1
+        B = newB
+    showboard(B, r, c, ' ', pause)
+    print('iterations', itn)
 
-interact(1200)
+
+def main():
+    interact(1200)
+
+
+if __name__ == '__main__':
+    main()
