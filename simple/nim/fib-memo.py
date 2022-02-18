0bb9c4fd0782761cdec304c9efeabc2d66860de6
hayward <hayward@ualberta.ca>
2022-02-17 21:46:00 -0700

    terser fib-memo

--- simple/nim/fib-memo.py
+++ simple/nim/fib-memo.py
@@ -1,21 +1,25 @@
 '''
-recursive fibonacci, with memoization:
-  avoid recomputation with a dictionary
+recursive fibonacci
+  * efficient version: avoid recomputation
+  * memoize with a dictionary
 '''
+def fib(n, D):
+  if n in D:
+    return D[n]
+  else:
+    f = fib(n-1, D) + fib(n-1, D)
+    # what happens if you remove the next line ?
+    D[n] = f  
+    return f
 
+'''
+initialize dictionary
+'''
 fib_values = {0:0, 1:1} 
 
-def fib(n, D):
-  if n <= 1:
-    return n
-  f = [0, 0]
-  for j in range(1,3):
-    if n - j in D:
-      f[j - 1] = D[n - j]
-    else:
-      f[j - 1] = fib(n - j, D)
-      D[n - j] = f[j - 1]   # what happens if you forget this line?
-  return sum(f)
-
-for j in range(40):
+'''
+compute 
+'''
+n = 100
+for j in range(n):
   print(j, fib(j, fib_values))
