c528e9e81328d0d68839a296e737cf8da9af3937
hayward <hayward@ualberta.ca>
2022-03-17 15:54:03 -0600

    also show number recursive calls, dict size

---
+++ simple/nim/nim-memo-calls.py
@@ -0,0 +1,48 @@
+# compute nim values using negamax and a dictionary
+#   that holds values already computed     RBH 2019
+#   2022: also show total number of recursive calls, dictionary size
+
+def get_piles():
+   while True:
+       raw = input('nim game pile sizes (eg. 3 5 7)   ')
+       try:
+          dim = tuple( int(x) for x in raw.split() )
+          if len(dim) > 0 and all(d >= 0 for d in dim):
+             return dim
+       except ValueError:
+          pass
+       print('invalid, try again')
+
+"""
+sd         dictionary(state: boolean), true if player-to-move wins
+"""
+def winning(nim_psn, sd, depth):  # tuple, dictionary, recursion depth
+  #print('  '*depth, nim_psn)
+  calls = 1
+  if nim_psn in sd:
+    return sd[nim_psn], calls
+  # nim_psn not in dictionary, so update before we return
+  if all(p == 0 for p in nim_psn): # we lose if every pile empty
+    sd.update({ nim_psn: False })  # update before return
+    return False, calls
+  psn = tuple(sorted(nim_psn))
+  for j in range(len(psn)): # each pile
+    if j == 0 or psn[j] > psn[j-1]:
+      for k in range(psn[j]): # number of stones that will remain in that pile
+        child = tuple(sorted(psn[:j] + (k,) + psn[j+1:]))
+        result = winning(child, sd, depth+1)
+        calls += result[1]
+        if not result[0]:
+          sd.update({ nim_psn: True })   # update before return
+          if depth == 0: 
+            print('\nwinning move to ',child, ' ', calls, 'calls')    # show a winning move
+          return True, calls
+  sd.update({ nim_psn: False })  # update before return
+  if depth == 0: 
+    print('\nlosing,', calls, 'calls')
+  return False, calls
+
+v = get_piles()
+S = dict()
+w = winning(v,S,0)
+print('dictionary size', len(S))
