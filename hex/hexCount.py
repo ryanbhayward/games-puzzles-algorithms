76c85661f3c56561f1cde3090c53d58788e2aac2
Ryan Hayward <hayward@ualberta.ca>
2022-11-09 09:16:17 -0800

    hexCount => python3

--- hex/hexCount.py
+++ hex/hexCount.py
@@ -8,35 +8,35 @@ def factorial(n):
 
 def choose(n, k):
     if 0 <= k <= n:
-	p = 1
-	for t in xrange(min(k, n - k)):
-            p = (p * (n - t)) // (t + 1)
-	return p
+        p = 1
+        for t in range(min(k, n - k)):
+           p = (p * (n - t)) // (t + 1)
+        return p
     else:
-	return 0
+        return 0
 	
 def pascalTriangle(n):
     for x in range(n + 1):
         for y in range(x + 1):
-	    print choose(x, y),
-        print ''
-    print ''
+            print(choose(x, y),end='')
+        print('')
+    print('')
 
 def boardStates(n, k):
 #   number of different (up to 180 degree rotation) k-stone nxn hex states
 #       can be deduced from the Tromp formulas in Browne's book
 
 #   exact answer if we ignore rotational symmetry:
-    xTerm  = choose(n * n, (k + 1) / 2) * choose(n * n - (k + 1) / 2,  k / 2)
+    xTerm  = choose(n * n, (k + 1) // 2) * choose(n * n - (k + 1) // 2,  k // 2)
 #   adjust for symmetry 
-    kd4 = k / 4 ; sTerm = 0
+    kd4 = k // 4 ; sTerm = 0
     if 0 == k % 4 or \
        1 == k % 4 and 1 == n % 2:
-        sTerm = choose(n * n / 2, kd4) * choose(n * n / 2 - kd4, kd4)
+        sTerm = choose(n * n // 2, kd4) * choose(n * n // 2 - kd4, kd4)
     if 3 == k % 4 and 1 == n % 2:
-        sTerm = choose(n * n / 2, kd4) * choose(n * n / 2 - kd4, 1 + kd4)
+        sTerm = choose(n * n // 2, kd4) * choose(n * n // 2 - kd4, 1 + kd4)
 #   each state now counted twice, so ...
-    return (xTerm + sTerm) / 2
+    return (xTerm + sTerm) // 2
 
 def uptoStates(n, s):
 #   boardStates with at most s stones
@@ -51,14 +51,14 @@ def upperBound(n):
     return (3**(n * n) + 1) / 2
 
 def showCounts(n):
-    print `n` + 'x' + `n`, 'Hex states'
+    print(n,'x',n, 'Hex states')
     accum = 0
     for k in range(n * n + 1):
-	b = boardStates(n, k)
+        b = boardStates(n, k)
         accum = accum + b
-	print k, k % 4, b
-    print '=', accum
-    print ' ', upperBound(n), '\n'
+        print(k, k % 4, b)
+    print('=', accum)
+    print(' ', upperBound(n), '\n')
 
 def nodeCount7x7():
 # number of nodes in 7x7 solver recursion tree, cf TCS paper
@@ -74,10 +74,10 @@ for n in range(1, 11):
     #print n, '\n', 1.0*uptoStates(n, n*n/2), '\n', uptoStates(n, n*n), '\n', upperBound(n), '\n'
     #print n, '\n', 1.0*uptoStates(n, n*n/2)
     #print n, '\n', uptoStates(n, n*n/2)
-    print n, '\n', uptoStates(n, n*n)
-    print ''
+    print(n, '\n', uptoStates(n, n*n))
+    print( '')
 
-#print '7x7 node count', nodeCount7x7()
+print('7x7 node count', nodeCount7x7())
 #for x in range(7):
 	#print 'full board states', x,'x',x,' board: ',boardStates(x,x*x)
 
