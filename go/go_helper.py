3c68184ea7cff358501d90e92ac8deced95ba0dc
hayward <hayward@ualberta.ca>
2023-01-09 17:04:19 -0700

    almost done

--- go/go_helper.py
+++ go/go_helper.py
@@ -6,15 +6,8 @@
   * allow rectangular boards
              1 <= R <= 19 rows 
              1 <= C <= 19 columns
-  in progress
-  * out.gdg just showing empty board: fix this
-  * allow read game from sgf (use python argparser)
-      - use -f feature to read sgf
-      - in this case also create a moves_board
-        that for each point shows last move there
-  * make history part of game_state
   todo * allow input illegal position, report whether position is legal
-       * put everything inside Game_state
+       * put more inside Game_state ?
 """
 
 """
@@ -34,12 +27,39 @@ guarded board --> g  g  g  g  g   20 21 22 23 24  <-- indices of board points
 from argparse import ArgumentParser
 
 """
-points, board
+input, output
 """
 
 IO_CHARS = '.*oge'  # point characters: empty, black, white, guard
 EMPTY, BLACK, WHITE, GUARD, ERASE = IO_CHARS[0], IO_CHARS[1], IO_CHARS[2], IO_CHARS[3], IO_CHARS[4]
 COLUMNS = 'ABCDEFGHJKLMNOPQRST'
+def char_to_color(c): 
+  return IO_CHARS.index(c)
+
+escape_ch   = '\033['
+colorend    =  escape_ch + '0m'
+textcolor   =  escape_ch + '0;37m'
+stonecolors = (textcolor,\
+               escape_ch + '0;35m',\
+               escape_ch + '0;32m',\
+               textcolor)
+
+def paint(s):  # s   a string
+  if len(s) > 1 and s[0] == ' ': 
+   return ' ' + paint(s[1:])
+  x = IO_CHARS.find(s[0])
+  if x > 0:
+    return stonecolors[x] + s + colorend
+  elif s.isalnum():
+    return textcolor + s + colorend
+  return s
+
+def menu():
+  m =  '\n  ' + BLACK + ' b2         play BLACK b 2'
+  m += '\n  ' + WHITE + ' e3         play WHITE e 2'
+  m += '\n  ' + EMPTY + ' d4         erase stone at d 4'
+  m += '\n   u                undo'
+  return m + '\n[return]            quit\n'
 
 def opponent(color): 
   if color == BLACK: 
@@ -90,6 +110,33 @@ class Game_state: # go board, each point in {B, W, E, G}
     self.labels_brd = [0]*self.guarded_n # label each point with number of last move there
     self.next_to_move = BLACK
 
+  def score_difference(self, score):
+    return score[0] + score[1] - (score[2] + score[3])
+
+  def score_msg(self): # score
+    tts = self.tromp_taylor_score()
+    sd = self.score_difference(tts)
+    msg = 'score ' + str(tts) 
+    if sd == 0:
+      msg += ': tied'
+    elif sd > 0:
+      msg += ': black winning by ' + str( sd)
+    else:
+      msg += ': white winning by ' + str(-sd)
+    return msg + '\n'
+
+  def report(self):
+    msg = 'move labels board\n'
+    msg += self.labels_msg()
+    msg += '\n' + self.score_msg()
+    with open('out.gdg', 'w', encoding="utf-8") as f:
+      f.write(msg)
+
+  def status_report(self):
+    self.showboard()
+    self.report()
+    print(self.score_msg())
+
   def actions_msg(self):
     print('actions: ', end='')
     msg = ''
@@ -129,10 +176,13 @@ class Game_state: # go board, each point in {B, W, E, G}
       while True:
         a = p.actions.pop()
         k, c, w = a.kind, a.color, a.where
-        if k == Game_state.StoneCapture: # capture move, restore it
+        if k == p.StoneCapture: # undo capture: restore
           p.brd = change_string(p.brd, w, c)
           # stay in loop in case more stones were captured
-        else: # normal move, erase it
+        elif k == p.Erase: # undo erase: restore
+          p.brd = change_string(p.brd, w, c)
+          return
+        else: # undo stoneput: erase
           p.brd = change_string(p.brd, w, EMPTY)
           # normal move, so only one stone to erase, we are done
           print(p.actions_msg())
@@ -151,6 +201,16 @@ class Game_state: # go board, each point in {B, W, E, G}
             return move_is_ok
           else:
             where = brd_index(x, y, self.C)
+            if color == EMPTY: # erase move
+              point_color = self.brd[where]
+              if point_color == EMPTY:
+                print('\n  that location is already empty')
+                return move_is_ok
+              else:
+                move_record = Action(self.Erase, point_color, where)
+                self.actions.append(move_record) # record move for undo
+                self.brd = change_string(self.brd, where, EMPTY) 
+                return True
             if self.brd[where] != EMPTY:
               print('\n  sorry, position occupied')
               return move_is_ok
@@ -232,107 +292,46 @@ class Game_state: # go board, each point in {B, W, E, G}
       msg += '\n'
     return msg
 
-"""
-input, output
-"""
-
-def char_to_color(c): 
-  return IO_CHARS.index(c)
-
-escape_ch   = '\033['
-colorend    =  escape_ch + '0m'
-textcolor   =  escape_ch + '0;37m'
-stonecolors = (textcolor,\
-               escape_ch + '0;35m',\
-               escape_ch + '0;32m',\
-               textcolor)
-
-def menu():
-  m =  '\n  ' + BLACK + ' b2         play BLACK b 2'
-  m += '\n  ' + WHITE + ' e3         play WHITE e 2'
-  m += '\n  ' + ERASE + BLACK + ' a4         erase black stone at e 2'
-  m += '\n   u                undo'
-  return m + '\n[return]            quit\n'
-
-def showboard(psn):
-  def paint(s):  # s   a string
-    if len(s) > 1 and s[0] == ' ': 
-     return ' ' + paint(s[1:])
-    x = IO_CHARS.find(s[0])
-    if x > 0:
-      return stonecolors[x] + s + colorend
-    elif s.isalnum():
-      return textcolor + s + colorend
-    return s
-
-  pretty = '\n    ' 
-  for c in range(psn.C): # columns
-    pretty += ' ' + paint(COLUMNS[c])
-  pretty += '\n'
-  for j in reversed(range(psn.R)): # rows
-    pretty += ' ' + paint('{:2d}'.format(1+j)) + ' '
-    for k in range(psn.C): # columns
-      pretty += ' ' + paint(psn.brd[brd_index(j,k,psn.C)])
+  def showboard(self):
+    pretty = '\n    ' 
+    for c in range(self.C): # columns
+      pretty += ' ' + paint(COLUMNS[c])
     pretty += '\n'
-  print(pretty)
+    for j in reversed(range(self.R)): # rows
+      pretty += ' ' + paint('{:2d}'.format(1+j)) + ' '
+      for k in range(self.C): # columns
+        pretty += ' ' + paint(self.brd[brd_index(j,k,self.C)])
+      pretty += '\n'
+    print(pretty)
 
-def score_difference(score):
-  return score[0] + score[1] - (score[2] + score[3])
-
-def score_msg(p): # score
-  tts = p.tromp_taylor_score()
-  sd = score_difference(tts)
-  msg = 'score ' + str(tts) 
-  if sd == 0:
-    msg += ': tied'
-  elif sd > 0:
-    msg += ': black winning by ' + str( sd)
-  else:
-    msg += ': white winning by ' + str(-sd)
-  return msg + '\n'
-
-def report(p):
-  msg = 'move labels board\n'
-  msg += p.labels_msg()
-  msg += '\n' + score_msg(p)
-  with open('out.gdg', 'w', encoding="utf-8") as f:
-    f.write(msg)
-
-def status_report(p):
-  showboard(p)
-  report(p)
-  print(score_msg(p))
-
-def play_from_sgf():
-  p = Game_state(19, 19)
-
-def interact(p):
-  while True:
-    status_report(p)
-    cmd = input('')
-    if len(cmd) == 0:
-      p.generate_labels()
-      print('\n ... adios :)\n')
-      print('\n labels\n' + p.labels_msg())
-      return
-    if cmd[0][0] == 'h':
-      print(menu())
-    elif cmd[0][0] == 'u':
-      p.undo_last_action()
-      if len(p.history) > 1: 
-        p.history.pop()
-    elif (cmd[0][0] in IO_CHARS):
-      sofar = p.requestmove(cmd)
-      if sofar: # no liberty violation, check superko
-        new_position = p.brd
-        if new_position in p.history:
-          print('superko violation, move not allowed')
-          p.undo_last_action()
-        else:
-          p.history.append(new_position)
-    else:
-      print('\n ???????\n')
-      print(menu())
+  def interact(self):
+    p = self
+    while True:
+      p.status_report()
+      cmd = input('')
+      if len(cmd) == 0:
+        p.generate_labels()
+        print('\n ... adios :)\n')
+        print('\n labels\n' + p.labels_msg())
+        return
+      if cmd[0][0] == 'h':
+        print(menu())
+      elif cmd[0][0] == 'u':
+        p.undo_last_action()
+        if len(p.history) > 1: 
+          p.history.pop()
+      elif (cmd[0][0] in IO_CHARS):
+        sofar = p.requestmove(cmd)
+        if sofar: # no liberty violation, check superko
+          new_position = p.brd
+          if new_position in p.history:
+            print('superko violation, move not allowed')
+            p.undo_last_action()
+          else:
+            p.history.append(new_position)
+      else:
+        print('\n ???????\n')
+        print(menu())
 
 def sgf_index(xy, C): # sgf index xy is row y (from bottom, alpha) column x
   return brd_index(ord(xy[1]) - ord('a'), ord(xy[0]) - ord('a'), C)
@@ -350,7 +349,7 @@ def parse_token(t): # return whether-move-is-valid and move
   if len(t) < 2:
     return False, ''
   if (t[0] == 'B' or t[0] == 'W') and t[1] == '[':
-    if t[2] == ']': # B[] or W[] is pass move
+    if t[2] == ']': # missing location indicates pass move
       return True, t[0] + '[]  '
     else:
       assert(t[4] == ']')
@@ -366,9 +365,7 @@ def parse_sgf(infile, C):
       if x[0]:
         (col, whr) = color_where(x[1], C)
         M.append([movenum, col, whr])
-        #print('{:3d}'.format(movenum), x[1], col, whr)
         movenum += 1
-  #print('')
   return M
 
 if __name__ == "__main__":
@@ -384,20 +381,20 @@ if __name__ == "__main__":
     p = Game_state(19,19)
     for mv in M:
       print(mv)
-      p.generate_labels(moves_list)
-      status_report(p, moves_list)
+      p.generate_labels()
+      p.status_report()
       color, where = mv[1], mv[2]
-      move_record = p.action(StonePut, color, where)
+      move_record = Action(p.StonePut, color, where)
       if color != EMPTY:  # ignore pass moves
         captured, move_is_ok = p.makemove(where, color)
         assert move_is_ok, 'invalid move from sgf game'
         for x in captured: # record captured stones for undo
-          cap_action = p.action(StoneCapture, opponent(color), x)
-          history.append(cap_action)
+          cap_action = Action(p.StoneCapture, opponent(color), x)
+          p.history.append(cap_action)
         new_position = p.brd
-        assert new_position not in history, 'superko violation from sgf game'
-      history.append(move_record) # record move for undo
-    showboard(p)
+        assert new_position not in p.history, 'sgf input: superko violation'
+      p.actions.append(move_record) # record move for undo
+    p.showboard()
   else:
     p = Game_state(2, 3)
-  interact(p)
+  p.interact()
