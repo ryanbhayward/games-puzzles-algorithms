a7f0bbc74979dbc8371d36d1bb37f1ba15ebd881
Ryan Hayward <hayward@ualberta.ca>
2024-04-05 12:42:21 -0600

    colors now characters, not indices

--- trigo/trigo.py
+++ trigo/trigo.py
@@ -4,9 +4,95 @@
   * they both use hexgo.py, stone_board.py
 """
 
-from trigo_utils import Cell, Color, IO, Pt, Board
+from trigo_utils import Cell, Color, IO, Board
 from time import time
 
+def menu():
+  m =  '\n  ' + BLACK + ' 2         play BLACK cell 2'
+  m += '\n  ' + WHITE + ' 0         play WHITE cell 0'
+  m += '\n  ' + EMPTY + ' 1         erase stone at cell 1'
+  m += '\n   u                undo'
+  return m + '\n[return]            quit\n'
+
+def change_string(p, where, ch):
+  return p[:where] + ch + p[where+1:]
+
+class Game_state: 
+  def __init__(self, r, c):
+    self.history = [self.brd]            # history of board positions
+    self.next_to_move = BLACK
+
+  def status_report(self):
+    self.showboard()
+    print(self.score_msg())
+
+  def requestmove(self, cmd):
+    move_is_ok, cmd = False, cmd.split()
+    if len(cmd)==2:
+      color = cmd[0][0]
+      if color in {BLACK, WHITE, EMPTY}:
+        q = cmd[1][0]
+        if q.isdigit():
+          if q<0 or q >= 3:
+            print('\n  sorry, cells 0, 1, 2 only')
+            return move_is_ok
+          else:
+            where = q
+            if color == EMPTY: # erase move
+              point_color = self.brd[where]
+              if point_color == EMPTY:
+                print('\n  that location is already empty')
+                return move_is_ok
+              else:
+                self.brd = change_string(self.brd, where, EMPTY) 
+                return True
+            if self.brd[where] != EMPTY:
+              print('\n  sorry, position occupied')
+              return move_is_ok
+            else:
+              print('\n ok we should do something')
+              if move_is_ok:
+                return move_is_ok
+
+  def interact(self):
+    p = self
+    while True:
+      p.status_report()
+      cmd = input('')
+      if len(cmd) == 0:
+        p.generate_labels()
+        p.export_gdg()
+        print('\n ... adios :)\n')
+        return
+      if cmd[0][0] == 'h':
+        print(menu())
+      elif cmd[0][0] == 'u':
+        p.undo_last_action()
+        if len(p.history) > 1: 
+          p.history.pop()
+      elif (cmd[0][0] in IO_CHR):
+        sofar = p.requestmove(cmd)
+        if sofar: # no liberty violation, check superko
+          new_position = p.brd
+          if new_position in p.history:
+            print('superko violation, move not allowed')
+            p.undo_last_action()
+          else:
+            p.history.append(new_position)
+      else:
+        print('\n ???????\n')
+        print(menu())
+
+def BW_to_PT(c):
+  return IO_CHR[' BW'.index(c)]
+
+def color_where(x, C):
+  if x[2] == ']':
+    return (EMPTY, 0) # pass move
+  else:
+    return (BW_to_PT(x[0]))
+
 start_time = time()
+Cell.test()
 IO.test()
 print('\ntime ', time() - start_time)
--- trigo/trigo_utils.py
+++ trigo/trigo_utils.py
@@ -8,21 +8,21 @@
 from string import ascii_lowercase
 
 class Cell: ############## board cells ###############
-  b, w, e, io_ch = 0, 1, 2, '*@.'  # black, white, empty
+  b, w, e = '*', '@', '.' # black, white, empty
+  io_ch = b+w+e
 
-  def from_ch(ch): return Cell.io_ch.index(ch)
-
-  def opponent(c): return 1 - c
+  def opponent(c): 
+    if c == Cell.e: return None
+    return Cell.b if c == Cell.w else Cell.w
 
   def test():
-    print('tests for class Cell')
-    io_ch = Cell.io_ch
-    for ch in io_ch:
-      c = Cell.from_ch(ch)
-      print(ch, c, io_ch[c])
+    print('\n  Cell test   ', end='')
+    for ch in Cell.io_ch: 
+      print(ch, '', end='')
+    print('\n   opponents  ', end='')
+    for j in Cell.io_ch: 
+      print(j, Cell.opponent(j), end='  ')
     print()
-    for j in range(2):
-      print(j, Cell.opponent(j))
 
 class Color: ############ for color output ############
   green   = '\033[0;32m'
@@ -41,31 +41,29 @@ class Color: ############ for color output ############
       else: p += c
     return p
 
-class IO:  ############## hex and go output #############
+class IO:  ############## input/output, strings #############
+
+  def change_string(p, where, ch):
+    return p[:where] + ch + p[where+1:]
 
   def spread(s): # embed blanks in string
     return ''.join([' ' + c for c in s])
   
-  def point_ch(stone_sets, p):
-    if p in stone_sets[0]: return Cell.io_ch[0]
-    if p in stone_sets[1]: return Cell.io_ch[1]
-    return Cell.io_ch[2]
+  def board_str(board):
+    s = '  ' + board[0] + '\n' + IO.spread(board[1:])
+    return Color.paint(s, Cell.io_ch)
 
-  def board_str(stone_sets, n):
-    return ''.join([IO.point_ch(stone_sets, p) for p in range(n)])
+  #def show_pairs(msg, d):
+  #  print('\n' + msg)
+  #  for x in d: print(x, d[x], end=' : ')
+  #  print()
 
-  def show_pairs(msg, d):
-    print('\n' + msg)
-    for x in d: print(x, d[x], end=' : ')
-    print()
-
-  def show_dict(msg, d):
-    print('\n' + msg)
-    for x in d: print(x, d[x])
+  #def show_dict(msg, d):
+  #  print('\n' + msg)
+  #  for x in d: print(x, d[x])
 
   def disp(state): 
-    s = '  ' + state.board[0] + '\n' + IO.spread(state.board[1:])
-    print(Color.paint(s, Cell.io_ch))
+    print(IO.board_str(state.board))
     print('      score ', state.score(), 'empty cells', state.empty_cells())
 
   def test():
@@ -84,18 +82,11 @@ class IO:  ############## hex and go output #############
     state.color_cell(Cell.w, 1)
     IO.disp(state)
 
-class Pt: ############## board points     ###############
-
-  def point_color(stones, p):
-    if p in stones[Cell.b]: return Cell.b
-    if p in stones[Cell.w]: return Cell.w
-    return Cell.e
-
 class Board:
-  n = 3        # only 3 cells :)
+  n = 3        # trigo board: only 3 cells   :)
 
   def __init__(self):
-    self.board = Cell.io_ch[Cell.e] * self.n
+    self.board = Cell.e * self.n
     self.history = [self.board]
     print('init board', self.board)
     print('init history', self.history)
@@ -103,29 +94,29 @@ class Board:
   def color_cell(self, color, where):
     assert(where in (0,1,2))
     assert(color in (Cell.b, Cell.w, Cell.e))
-    assert(color == Cell.e or self.board[where] == Cell.io_ch[Cell.e])
-    self.board = self.board[:where] + Cell.io_ch[color] + self.board[where+1:]
+    assert(color == Cell.e or self.board[where] == Cell.e)
+    self.board = IO.change_string(self.board, where,  color)
 
   def is_legal(self):
-    return Cell.io_ch[Cell.e] in self.board
+    return Cell.e in self.board
 
   def empty_cells(self):
-    empties = [j for j, x in enumerate(self.board) if x == Cell.io_ch[Cell.e]]
+    empties = [j for j, x in enumerate(self.board) if x == Cell.e]
     return empties
 
   def legal_moves(self, color):
     assert(color in (Cell.b, Cell.w))
     assert self.is_legal()
-    bcount = self.board.count(Cell.io_ch[Cell.b])
-    wcount = self.board.count(Cell.io_ch[Cell.w])
+    bcount = self.board.count(Cell.b)
+    wcount = self.board.count(Cell.w)
     return self.empty_cells()
 
   def score(self):
     if self.is_legal():
-      bcount = self.board.count(Cell.io_ch[Cell.b])
-      wcount = self.board.count(Cell.io_ch[Cell.w])
-      if bcount == 2 or bcount > wcount: return 1
-      if wcount == 2 or bcount < wcount: return -1
+      bcount = self.board.count(Cell.b)
+      wcount = self.board.count(Cell.w)
+      if bcount == 2 or bcount > wcount: return 3
+      if wcount == 2 or bcount < wcount: return -3
       return 0
     else:
       print('      illegal position ', end='')
