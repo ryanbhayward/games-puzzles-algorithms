57a7199140820dff971585017349d741f5023b41
Ryan Hayward <hayward@ualberta.ca>
2024-05-27 15:26:57 -0600

    used go_helper to print px of AGLS1 and katrain completion

--- go/go_helper.py
+++ go/go_helper.py
@@ -324,7 +324,8 @@ class Game_state: # go board, each point in {B, W, E, G}
     for c in range(self.C): # columns
       pretty += ' ' + paint(COLUMNS[c])
     pretty += '\n'
-    for j in reversed(range(self.R)): # rows
+    #for j in reversed(range(self.R)): # rows, asian
+    for j in range(self.R): # rows, western (e.g. deepmind AGLS series)
       pretty += ' ' + paint('{:2d}'.format(1+j)) + ' '
       for k in range(self.C): # columns
         pretty += ' ' + paint(self.brd[brd_index(j,k,self.C)])
