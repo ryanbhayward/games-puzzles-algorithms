714e3d809e7e8de370c023db6b763006e7affb2a
hayward <hayward@ualberta.ca>
2025-03-15 19:33:28 -0600

    closer

--- linear/lcb.py
+++ linear/lcb.py
@@ -1,6 +1,7 @@
 # simple program to play linear clobber
 import sys
 from paint_chars import paint3
+from collections import deque
 #from copy import deepcopy
 
 BLACK, WHITE, EMPTY = 0,1,2
@@ -23,22 +24,45 @@ def show(brd):
   #print(paint3(indexstr + '\n' + cellstr + '\n' + brd, CHARS))
   print(paint3('\n' + indexstr + '\n' + cellstr, CHARS))
 
+def reverse(s):
+  return s[::-1]
+
+def form(s, pattern, symbol):
+  p = s.replace(pattern, symbol)
+  while symbol*2 in p:
+    p = p.replace(symbol*2, symbol)
+  return p
+
+def myadd(item, s):
+  if item not in s and reverse(item) not in s: 
+    s.add(item)
+
 def clobber(brd):
   n = len(brd)
+  newset = set()
   for j in range(2):
     stone = CHARS[j]
-    print('\n', paint3(stone, CHARS), ' clobbers forward\n', sep='')
+
+    #print('\n', paint3(stone, CHARS), ' clobbers forward\n', sep='')
     for k in range(n-1):
       if (brd[k] == stone) and (brd[k] != brd[k+1]):
         b0, b1 = brd[:k], brd[k] + brd[k+2:]
-        print(k, paint3(b0, CHARS),  paint3(b1, CHARS))
-    print('\n', paint3(stone, CHARS), ' clobbers backwards\n', sep='')
+    #    print(k, paint3(b0, CHARS),  paint3(b1, CHARS))
+    #    b0, b1 = form(b0, 'xxo', 'g'), form(b1, 'xxo', 'g')
+    #    print(k, paint3(b0, CHARS),  paint3(b1, CHARS))
+        myadd(b0, newset)
+        myadd(b1, newset)
+
+    #print('\n', paint3(stone, CHARS), ' clobbers backwards\n', sep='')
     for k in range(1,n):
       if (brd[k] == stone) and (brd[k] != brd[k-1]):
         b0, b1 = brd[:k-1] + brd[k], brd[k+1:]
-        print(k, paint3(b0, CHARS),  paint3(b1, CHARS))
-        b0, b1 = b0.replace('xxo', 'g'), b1.replace('xxo', 'g')
-        print(k, paint3(b0, CHARS),  paint3(b1, CHARS))
+    #    print(k, paint3(b0, CHARS),  paint3(b1, CHARS))
+    #    b0, b1 = form(b0, 'xxo', 'g'), form(b1, 'xxo', 'g')
+    #    print(k, paint3(b0, CHARS),  paint3(b1, CHARS))
+        myadd(b0, newset)
+        myadd(b1, newset)
+  return newset
 
 class LC_state:
   def __init__(self):
@@ -75,5 +99,16 @@ def playGame(state):
 
 st = LC_state()
 show(st.b)
-clobber(st.b)
+q, spawn = [st.b], set()
+while len(q) > 0:
+  brd = q.pop(0)
+  newbrds = clobber(brd)
+  for b in newbrds:
+    if b not in spawn and reverse(b) not in spawn:
+      print(paint3(b, CHARS))
+      spawn.add(b)
+      q.append(b)
+print('\ndone')
+
+#clobber(st.b)
 #playGame(brd)
