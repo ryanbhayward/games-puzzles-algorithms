e15e2e25d193c198d0fde1b1c75fcd1ce1de7407
Ryan Hayward <hayward@ualberta.ca>
2025-05-05 14:32:03 -0600

    line numbers

--- hex/xin_mmx.py
+++ hex/xin_mmx.py
@@ -1,15 +1,15 @@
 # return winning move if ptm wins else empty string
-def mmx_move(psn, ptm): # assumes no winner yet
-  optm = oppCH(ptm) # opponent of player-to-move
-  for k in CELLS: # for every cell on the board
-    if is_empty_cell(k, psn): 
-      new_psn = color_cell(k, psn, ptm) 
-      if has_win(new_psn, ptm): # did ptm win?
-        return k
-      # if not, continue from new_psn with optm
-      optm_wins = mmx_move(new_psn, optm)
-      # if optm has no winning move, then
-      #   ptm move at k is a winning move
-      if not optm_wins:
-        return k
-  return '' # no winning move
+0 def mmx_move(psn, ptm): # assumes no winner yet
+1   optm = oppCH(ptm) # opponent of player-to-move
+2   for k in CELLS: # for every cell on the board
+3     if is_empty_cell(k, psn): 
+4       new_psn = color_cell(k, psn, ptm) 
+5       if has_win(new_psn, ptm): # did ptm win?
+6         return k
+7       # if not, continue from new_psn with optm
+8       optm_wins = mmx_move(new_psn, optm)
+9       # if optm has no winning move, then
+10      #   ptm move at k is a winning move
+11      if not optm_wins:
+12         return k
+13  return '' # no winning move
