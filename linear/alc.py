0c5d7c525152eed5c46d099a9a1273ddc4fc8251
Ryan Hayward <hayward@ualberta.ca>
2025-05-22 18:39:30 -0600

    working

--- linear/alc.py
+++ linear/alc.py
@@ -2,12 +2,9 @@
 # started 2025
 # rbh
 
-# TODO: clobber not working properly
-#   check that the start move has a color
-# and that the destination move is the opposite color
+# TODO: add pattern names
 
 import sys
-#import string
 from paint_chars import paint3
 from collections import deque
 from copy import copy
@@ -15,37 +12,27 @@ from copy import copy
 BLK, WHT, EMP = 0,1,2  # b LEFT, w RIGHT
 CHRS = 'xo_'  # black white g
 
+class ALC:
+  def __init__(self, k):
+    self.b =  'ox'*k
+
+def opp_ch(ch): 
+  cell = CHRS.index(ch)
+  assert(ch != EMP)
+  return CHRS[BLK] if cell == WHT else CHRS[WHT]
+
+def change_str(brd, psn, ch):
+  return brd[:psn] + ch + brd[psn+1:]
+
+def revstring(s): return s[::-1]
+
 def direction(right):
   return 'right' if right else 'left '
 
-def opp_ch(ch): return 'x' if ch == 'o' else 'o'
-
-def opponent(col): return 1 - col
-
-def color(brd, psn, color):
-  return brd[:psn] + CHRS[color] + brd[psn+1:]
-
-def undo(H, brd):  # pop last meta-move
-  if len(H)==1:
-    print('\n    original position,  nothing to undo\n')
-    return brd
-  else:
-    H.pop()
-    return copy(H[len(H)-1])
-
-def clobber(brd, psn, n, rgt): # if rgt, clobber right
-  print(psn, 'clobber', direction(rgt), end='  ')
-  delta = 1 if rgt else -1
-  if (psn + delta < 0) or (psn + delta >= n):
-    print('off board')
-    return ''
-  bp, bpd = brd[psn], brd[psn + delta]
-  if (bp == EMP or bpd == EMP or bp == bpd):
-    print('invalid there')
-    return ''
-  b1 = color(brd, psn, EMP)
-  col = CHRS.index(b1[psn + delta])
-  return color(b1, psn + delta, opponent(col))
+def clobber(brd, psn, delta):
+  b = change_str(brd, psn + delta, brd[psn])
+  b = change_str(b,   psn,        CHRS[EMP])
+  return b
 
 def show(brd):
   indexstr = '   '  # print cell indices
@@ -56,46 +43,50 @@ def show(brd):
   cellstr = '   ' + ''.join(['  ' + c for c in brd])
   print(paint3('\n' + indexstr + '\n' + cellstr, CHRS))
 
-def revstring(s): return s[::-1]
+def error(oops):
+  if oops == 'mv_format':
+    print(' enter move like this    13+')
+    print('              or this     0-')
+  elif oops == 'off_board':
+    print(' invalid: off board')
+  elif oops == 'empty':
+    print(' that location empty')
+  elif oops == 'nbr':
+    print(' stone must touch nbr')
+  elif oops == 'nbr_emp':
+    print(' neighbor empty')
+  else:
+    print(' ??? unknown error')
+  return ''
 
-def myadd(item, s):
-  if item not in s and revstring(item) not in s: 
-    s.add(item)
-
-def nonzero(brd):
-  for j in range(len(brd)-1):
-    if brd[j] != EMP and \
-       brd[j+1] != EMP and \
-       brd[j] == opp_ch(brd[j+1]): return True
-  return False
-
-class ALC:
-  def __init__(self, k):
-    self.b =  'ox'*k
-
-def show_format():
-  print('invalid move format')
-  print('  like this: 13+')
-  print('... or this: 0-')
-
-def clbbr(brd, psn, delta):
-  b1 = color(brd, psn, EMP)
-  col = CHRS.index(b1[psn + delta])
-  return color(b1, psn + delta, opponent(col))
+def undo(H, brd):  # pop last meta-move
+  if len(H)==1:
+    print('\n    original position,  nothing to undo\n')
+    return brd
+  else:
+    H.pop()
+    return copy(H[len(H)-1])
 
 def requestmove(brd, cmd):
   parseok = False
+  n = len(brd)
   psn, direction = cmd[:-1], cmd[-1]
   if (cmd[-1] not in '+-') or not psn.isdigit():
-    show_format()
-    return ''
+    return error('mv_format')
   psn = int(psn)
+  if psn < 0 or psn >= n:
+    return error('off_board')
+  if brd[psn] == CHRS[EMP]:
+    return error('empty')
   if psn == 0 and direction == '-' or \
-     psn >= len(brd) - 1 and direction == '+':
-    print('invalid move: off board')
-    return ''
-  delta = 1 if direction else -1
-  return clbbr(brd, psn, delta)
+     psn >= n - 1 and direction == '+':
+    return error('off_board')
+  delta = 1 if direction=='+' else -1
+  if brd[psn] == CHRS[EMP]:
+    return error('nbr_emp')
+  if brd[psn] != opp_ch(brd[psn + delta]):
+    return error('nbr')
+  return clobber(brd, psn, delta)
 
 def get_command(color):
   print('\n ' + color + '  command? ', end='')
@@ -117,44 +108,12 @@ def test(brd):
       cr = clobber(brd, j, n, (True,False)[k])
       if cr: print(paint3(cr, CHRS))
 
-def playGame(state):
-  while True:
-    show(state.b)
-    m = get_command(CHRS[WHT])
-    if m == -3:
-      break
-    elif m == -1:
-      print('sorry ? ... ')
-    elif m == 0:
-      test(state.b)
-    break
-  #print('\n  adios ...\n  zaijian ...\n  sayonara ...\n  annyeong ...\n')
-  print('\n  adios ...')
-
-def equal(a,b):
-  lena, lenb = len(a), len(b)
-  if lena != lenb:
-    print('unequal length lists', lena, lenb)
-    return False
-  for j in range(lena):
-    if a[j] != b[j]:
-      print('unequal at index',j, a[j], b[j])
-      return False
-  return True
-
-def mydiff(sa, sb):
-  for x in sa:
-    if x not in sb: print('in L0 not L1', x)
-  for x in sb:
-    if x not in sa: print('in L1 not L0', x)
-
 def interact(brd):
   history = []  # board positions
   new = copy(brd); history.append(new)
   while True:
     show(brd)
     cmd = input(' ')
-    print(cmd)
     if len(cmd)==0:
       print('\n ... adios :)\n')
       return
@@ -162,7 +121,7 @@ def interact(brd):
     #  printmenu()
     elif cmd[0][0]=='u':
       brd = undo(history, brd)
-    elif (cmd[0][0].isdigit()):
+    else:
       new = requestmove(brd, cmd)
       if new != '':
         brd = new
