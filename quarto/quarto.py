d05affb82e6977d72291a686feaefc3b5aabe26d
Ryan Hayward <hayward@ualberta.ca>
2025-12-30 13:47:09 -0700

    working?

--- quarto/quarto.py
+++ quarto/quarto.py
@@ -3,8 +3,14 @@
 # - represent quarto state as sequence of 16 4-bit integers
 # - TODO simulate random games
 
-#STATE = tuple(16*[-1])
-STATE = tuple(range(16))
+STATE = 16*[-1]
+#STATE = list(range(16))
+
+STATE[3]  = 13
+STATE[6]  =  0
+STATE[9]  =  9
+STATE[12] =  5
+
 
 FILES = ((0,1,2,3),(4,5,6,7),(8,9,10,11),(12,13,14,15),
          (0,4,8,12),(1,5,9,13),(2,6,10,14),(3,7,11,15),
@@ -21,15 +27,18 @@ def show(st):
     if (k%4) == 3:
       print()
 
+def same_bit(x,y,psn):
+  return (x >> psn) &1 == (y >> psn) &1
+
 def wins(f):
   file = FILES[f]
   for j in range(4):
     if STATE[file[j]] < 0: 
       return False
-  for power in [2,4,8,16]:
-    if STATE[file[0]]%power == STATE[file[1]]%power and \
-       STATE[file[0]]%power == STATE[file[2]]%power and \
-       STATE[file[0]]%power == STATE[file[3]]%power:
+  for psn in range(3):
+    if same_bit(STATE[file[0]], STATE[file[1]], psn) and \
+       same_bit(STATE[file[0]], STATE[file[2]], psn) and \
+       same_bit(STATE[file[0]], STATE[file[3]], psn):
       return True
   return False
 
